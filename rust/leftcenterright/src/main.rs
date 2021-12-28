822e2844d0bf655e22c13e4bc33e9bcd1160ae70
warren <warren.weckesser@gmail.com>
2021-12-28 18:50:07 -0500

    rust: leftcenterright: Add parallel computation option

--- rust/leftcenterright/src/main.rs
+++ rust/leftcenterright/src/main.rs
@@ -1,10 +1,10 @@
 // Left-center-right dice game simulation.
 // Copyright (c) 2021 Warren Weckesser
 
-use std::cmp;
 use rand::Rng;
+use std::cmp;
+use std::thread;
 use structopt::StructOpt;
-
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "leftcenterright", about = "Simulate left-center-right game.")]
@@ -14,21 +14,32 @@ struct Opt {
     players: usize,
 
     /// Starting number of chips.
-    #[structopt(short = "s", long = "start", default_value="3")]
+    #[structopt(short = "s", long = "start", default_value = "3")]
     start: usize,
 
     /// Number of games to simulate.
     #[structopt(short = "g", long = "games", default_value = "1000")]
     games: usize,
+
+    // Number of parallel jobs (i.e. threads) to use; 0 implies serial.
+    #[structopt(short = "j", long = "jobs", default_value = "0")]
+    jobs: usize,
 }
 
-
 fn next(i: usize, n: usize) -> usize {
-    if (i + 1) == n {0} else {i + 1}
+    if (i + 1) == n {
+        0
+    } else {
+        i + 1
+    }
 }
 
 fn prev(i: usize, n: usize) -> usize {
-    if i == 0 {n - 1} else {i - 1}
+    if i == 0 {
+        n - 1
+    } else {
+        i - 1
+    }
 }
 
 fn count_nonzero(v: &[usize]) -> usize {
@@ -46,15 +57,15 @@ fn sim(players: usize, start: usize) -> usize {
             match roll {
                 0 => {
                     chips[turn] -= 1;
-                    chips[prev(turn, players)] += 1;                    
-                },
+                    chips[prev(turn, players)] += 1;
+                }
                 1 => {
                     chips[turn] -= 1;
-                },
+                }
                 2 => {
                     chips[turn] -= 1;
-                    chips[next(turn, players)] += 1;  
-                },
+                    chips[next(turn, players)] += 1;
+                }
                 _ => {}
             }
         }
@@ -64,21 +75,50 @@ fn sim(players: usize, start: usize) -> usize {
     chips.iter().position(|&n| n > 0).unwrap()
 }
 
-fn stats(opt: &Opt) -> Vec<usize> {
-    let mut wins = vec![0; opt.players];
-    for _n in 0..opt.games {
-        let winner = sim(opt.players, opt.start);
+fn stats(players: usize, start: usize, games: usize) -> Vec<usize> {
+    let mut wins = vec![0; players];
+    for _n in 0..games {
+        let winner = sim(players, start);
         wins[winner] += 1;
     }
     wins
 }
 
+fn parallel_stats(opt: &Opt) -> Vec<usize> {
+    // (b, r) = divmod(opt.games, opt.jobs)
+    let b = opt.games / opt.jobs;
+    let r = opt.games % opt.jobs;
+    let batchsizes: Vec<usize> = (0..opt.jobs).map(|k| b + (k < r) as usize).collect();
+
+    let mut children = vec![];
+
+    for i in 0..opt.jobs {
+        let players = opt.players;
+        let start = opt.start;
+        let batchsize = batchsizes[i];
+        children.push(thread::spawn(move || stats(players, start, batchsize)));
+    }
+
+    let mut wins = vec![0; opt.players];
+    for child in children {
+        let batch_wins = child.join().unwrap();
+        for i in 0..opt.players {
+            wins[i] += batch_wins[i];
+        }
+    }
+    wins
+}
 
 fn main() {
     let opt = Opt::from_args();
     println!("opt.players : {}", opt.players);
     println!("opt.start   : {}", opt.start);
+    println!("opt.jobs    : {}", opt.jobs);
     println!("opt.games   : {}", opt.games);
-    let wins = stats(&opt);
+    let wins = if opt.jobs == 0 {
+        stats(opt.players, opt.start, opt.games)
+    } else {
+        parallel_stats(&opt)
+    };
     println!("{:?}", wins);
 }
