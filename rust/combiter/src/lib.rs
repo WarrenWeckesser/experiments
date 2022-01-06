68815efa2d834cb6cb74d2b7299ffc10c856932b
Warren Weckesser <warren.weckesser@gmail.com>
2022-01-06 15:50:18 -0500

    rust: implement a variation of the combinations iterator

--- rust/combiter/src/lib.rs
+++ rust/combiter/src/lib.rs
@@ -1,27 +1,32 @@
-/// The `CombIter` iterator iterates through all combinations
+/// The `CombIterArray` iterator iterates through all combinations
 /// of `TOTAL` objects distributed among `NBINS` bins.
 /// The iterated item has type `[usize; NBINS]`.
+
+// `CombIterArray` passes around arrays on the stack.  It will
+// likely crash if `NBINS` is big enough.  Each return value from
+// the `next()` method of the `Iterator` trait is a new array, not
+// a reference to the array stored in `CombIterArray`.
 
 // TODO: Ensure that the edge case NBINS=0 is handled properly
 // (which first requires deciding what that means).
 
-pub struct CombIter<const TOTAL: usize, const NBINS: usize> {
+pub struct CombIterArray<const TOTAL: usize, const NBINS: usize> {
     counts: [usize; NBINS],
 }
 
-impl<const TOTAL: usize, const NBINS: usize> CombIter<TOTAL, NBINS> {
+impl<const TOTAL: usize, const NBINS: usize> CombIterArray<TOTAL, NBINS> {
     pub fn new() -> Self {
         Self { counts: [0; NBINS] }
     }
 }
 
-impl<const TOTAL: usize, const NBINS: usize> Default for CombIter<TOTAL, NBINS> {
+impl<const TOTAL: usize, const NBINS: usize> Default for CombIterArray<TOTAL, NBINS> {
     fn default() -> Self {
         Self { counts: [0; NBINS] }
     }
 }
 
-impl<const TOTAL: usize, const NBINS: usize> Iterator for CombIter<TOTAL, NBINS> {
+impl<const TOTAL: usize, const NBINS: usize> Iterator for CombIterArray<TOTAL, NBINS> {
     type Item = [usize; NBINS];
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -73,6 +78,72 @@ impl<const TOTAL: usize, const NBINS: usize> Iterator for CombIter<TOTAL, NBINS>
     }
 }
 
+//
+// Alternate version
+//
+// `CombData` holds a `Vec`, so the actual data is on the heap.
+//
+// `CombData` does not implement `Iterator`.  Instead, it
+// has a `next()` method that must be called explicitly.
+// `next()` returns slices of the `Vec` held by the `CombData`.
+//
+// (My rust-fu is not strong enough to figure out how to use an
+// `Iterator` here.  In an `Iterator`, the values returned by
+// `next()` must outlive the `Iterator` object, so, for example,
+// they can't be slices of the `counts` vector.  Some other object
+// must hold the vector (or array), and it must outlive the `Iterator`.
+// A different approach that shows up while looking into this is
+// the "streaming iterator".)
+
+pub struct CombData<const TOTAL: usize, const NBINS: usize> {
+    counts: Vec<usize>,
+}
+
+impl<const TOTAL: usize, const NBINS: usize> CombData<TOTAL, NBINS> {
+    pub fn new() -> Self {
+        Self {
+            counts: vec![0usize; NBINS],
+        }
+    }
+
+    fn next<'a>(&'a mut self) -> Option<&'a [usize]> {
+        if self.counts[NBINS - 1] == TOTAL {
+            return None;
+        }
+        // Find the rightmost nonzero bin.
+        let mut rightmost_nonzero = NBINS - 1;
+        let mut found_nonzero = false;
+        for i in (0..NBINS).rev() {
+            if self.counts[i] != 0 {
+                rightmost_nonzero = i;
+                found_nonzero = true;
+                break;
+            }
+        }
+        if !found_nonzero {
+            // All the values in self.counts are 0.  This means
+            // this is the first call of next().  The first iterate
+            // is [TOTAL, 0, ..., 0].
+            self.counts[0] = TOTAL;
+            return Some(&self.counts);
+        }
+        if rightmost_nonzero != NBINS - 1 {
+            self.counts[rightmost_nonzero] -= 1;
+            self.counts[rightmost_nonzero + 1] += 1;
+        } else {
+            let mut j = rightmost_nonzero - 1;
+            while self.counts[j] == 0 {
+                j -= 1;
+            }
+            let p = self.counts[NBINS - 1];
+            self.counts[NBINS - 1] = 0;
+            self.counts[j + 1] = p + 1;
+            self.counts[j] -= 1;
+        }
+        Some(&self.counts)
+    }
+}
+
 // - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // Tests
 // - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -91,7 +162,7 @@ mod tests {
             [0, 1, 1],
             [0, 0, 2],
         ];
-        let ci = CombIter::<2, 3>::new();
+        let ci = CombIterArray::<2, 3>::new();
         for (c, expectedc) in ci.zip(expected.iter()) {
             assert_eq!(&c, expectedc);
         }
@@ -107,7 +178,7 @@ mod tests {
             [0, 1, 1],
             [0, 0, 2],
         ];
-        let ci = CombIter::<2, 3>::default();
+        let ci = CombIterArray::<2, 3>::default();
         for (c, expectedc) in ci.zip(expected.iter()) {
             assert_eq!(&c, expectedc);
         }
@@ -127,7 +198,7 @@ mod tests {
             [0, 0, 1, 1],
             [0, 0, 0, 2],
         ];
-        let ci = CombIter::<2, 4>::new();
+        let ci = CombIterArray::<2, 4>::new();
         for (c, expectedc) in ci.zip(expected.iter()) {
             assert_eq!(&c, expectedc);
         }
@@ -147,7 +218,7 @@ mod tests {
             [0, 1, 2],
             [0, 0, 3],
         ];
-        let ci = CombIter::<3, 3>::new();
+        let ci = CombIterArray::<3, 3>::new();
         for (c, expectedc) in ci.zip(expected.iter()) {
             assert_eq!(&c, expectedc);
         }
@@ -177,7 +248,7 @@ mod tests {
             [0, 0, 1, 2],
             [0, 0, 0, 3],
         ];
-        let ci = CombIter::<3, 4>::new();
+        let ci = CombIterArray::<3, 4>::new();
         for (c, expectedc) in ci.zip(expected.iter()) {
             assert_eq!(&c, expectedc);
         }
@@ -186,7 +257,7 @@ mod tests {
     #[test]
     fn test_basic_0_3() {
         let expected = [[0, 0, 0]];
-        let ci = CombIter::<0, 3>::new();
+        let ci = CombIterArray::<0, 3>::new();
         for (c, expectedc) in ci.zip(expected.iter()) {
             assert_eq!(&c, expectedc);
         }
@@ -195,7 +266,7 @@ mod tests {
     #[test]
     fn test_basic_5_1() {
         let expected = [[5]];
-        let ci = CombIter::<5, 1>::new();
+        let ci = CombIterArray::<5, 1>::new();
         for (c, expectedc) in ci.zip(expected.iter()) {
             assert_eq!(&c, expectedc);
         }
@@ -203,8 +274,26 @@ mod tests {
 
     #[test]
     fn test_last_5_4() {
-        let ci = CombIter::<5, 4>::new();
+        let ci = CombIterArray::<5, 4>::new();
         let last = ci.last().unwrap();
         assert_eq!(last, [0, 0, 0, 5]);
+    }
+
+    #[test]
+    fn test_combdata_2_3() {
+        let expected = [
+            [2, 0, 0],
+            [1, 1, 0],
+            [1, 0, 1],
+            [0, 2, 0],
+            [0, 1, 1],
+            [0, 0, 2],
+        ];
+        let mut ci = CombData::<2, 3>::new();
+        let mut i = 0;
+        while let Some(c) = ci.next() {
+            assert_eq!(c, expected[i]);
+            i += 1;
+        }
     }
 }
