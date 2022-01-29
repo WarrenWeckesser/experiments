fc637e111a46c98df4979232c4710832d7febeda
warren <warren.weckesser@gmail.com>
2022-01-29 16:04:53 -0500

    rust/rand_demos: demo generation of nan by sample method of dirichlet

---
+++ rust/rand_demos/dirichlet_demo/src/main.rs
@@ -0,0 +1,20 @@
+use rand;
+use rand::distributions::Distribution;
+use rand_distr::Dirichlet;
+
+fn main() {
+    let n = 1000;
+    println!("Checking {} Dirichlet samples", n);
+
+    let dirichlet = Dirichlet::new(&[0.001, 0.001, 0.001]).unwrap();
+    let mut r = rand::thread_rng();
+    let mut nancount = 0;
+    for _ in 0..n {
+        let sample: Vec<f64> = dirichlet.sample(&mut r);
+        println!("{:?}", sample);
+        if sample.iter().any(|x| x.is_nan()) {
+            nancount += 1;
+        }
+    }
+    println!("nancount: {}", nancount);
+}
