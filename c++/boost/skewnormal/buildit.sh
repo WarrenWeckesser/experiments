1e239e03bd2efaa7a50c9dc1ce9dc20bb1bdff86
warren <warren.weckesser@gmail.com>
2022-01-29 15:55:18 -0500

    c++/boost: Add skewnormal CDF example

---
+++ c++/boost/skewnormal/buildit.sh
@@ -0,0 +1,1 @@
+g++ -std=c++1z -Werror -Wall skewnormal_cdf.cpp -o skewnormal_cdf
---
+++ c++/boost/skewnormal/skewnormal_cdf.cpp
@@ -0,0 +1,26 @@
+
+#include <boost/math/distributions/skew_normal.hpp>
+using boost::math::skew_normal_distribution;
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    double loc = 0.0;
+    double scale =  1.0;
+    double shape[] = {2.0, 2.0, 5.0};
+    double x[] = {1.0, -4.0, -2.0};
+
+    int n = sizeof(x) / sizeof(x[0]);
+
+    cout << "        x           shape   cdf" << endl;
+    cout << scientific;
+    for (int i = 0; i < n; ++i) {
+        double c = cdf(skew_normal_distribution(loc, scale, shape[i]), x[i]);
+        cout << fixed << setw(12) << setprecision(4) << x[i] << " ";
+        cout << fixed << setw(12) << setprecision(4) << shape[i] << " ";
+        cout << scientific << setprecision(17) << c << endl;
+    }
+}
