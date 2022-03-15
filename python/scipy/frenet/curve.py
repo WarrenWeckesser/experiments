7f8dfc6b1a5ae87457fb9d4f4917523b4e871f76
Warren Weckesser <warren.weckesser@gmail.com>
2022-03-15 12:51:11 -0400

    python/scipy: In frenet/curve.py, simplify the ODE system
    Don't need to explictly solve for the binormal vector, since it
    can be computed from the tangent and normal vectors.

--- python/scipy/frenet/curve.py
+++ python/scipy/frenet/curve.py
@@ -13,12 +13,14 @@ import matplotlib.pyplot as plt
 # In the following, `s` is arclength.
 
 def curve_eqns(s, z, curvature, torsion):
+    # t and n are the tangent and normal vectors
     t = z[3:6]
     n = z[6:9]
-    b = z[9:]
+    # b is the binormal vector
+    b = np.cross(t, n)
     kappa = curvature(s)
     tau = torsion(s)
-    dzds = np.concatenate((t, kappa*n, -kappa*t + tau*b, -tau*n))
+    dzds = np.concatenate((t, kappa*n, -kappa*t + tau*b))
     return dzds
 
 
@@ -37,13 +39,12 @@ x0 = [0, 0, 0]
 t0 = [0, 1, 0]
 # Normal (a unit vector orthogonal to t0):
 n0 = [0, 0, 1]
-# Binornmal (completes the initial 3-d Frenet-Serret frame):
-b0 = np.cross(t0, n0)
-z0 = np.concatenate((x0, t0, n0, b0))
+
+z0 = np.concatenate((x0, t0, n0))
 
 s = np.linspace(-50, 50, 2000)
-use_odeint = True
 rtol = 5e-13
+use_odeint = False
 if use_odeint:
     sol = odeint(curve_eqns, z0, s, args=(curvature, torsion),
                  rtol=rtol, tfirst=True)
@@ -53,7 +54,12 @@ else:
                        rtol=rtol, method='Radau')
     sol = result.y.T
 
-ff = sol[-1, 3:].reshape((3, 3)).T
+tf = sol[-1, 3:6]
+nf = sol[-1, 6:9]
+bf = np.cross(tf, nf)
+# The final Frenet-Serret frame:
+ff = np.column_stack((tf, nf, bf))
+
 print("ff (the final Frenet-Serret frame):")
 print(ff)
 print()
@@ -66,6 +72,7 @@ print("1 - det(ff) =", 1 - np.linalg.det(ff),
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 ax.plot(sol[:, 0], sol[:, 1], sol[:, 2])
+
 # Simulate equal aspect ratio:
 ptp = sol[:, :3].ptp(axis=0)
 ax.set_box_aspect(ptp)
