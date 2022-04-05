ce6cfe5d12b1cd7d33bec25e859da6e3830411e3
Warren Weckesser <warren.weckesser@gmail.com>
2022-04-05 14:55:45 -0400

    c++: add doubledouble

---
+++ c++/doubledouble/demo.cpp
@@ -0,0 +1,58 @@
+
+#include <cstdio>
+#include "doubledouble.h"
+
+
+void
+print_doubledouble(const char *prefix, const DoubleDouble& x)
+{
+    printf("%s = (%24.17f, %24.17e)\n", prefix, x.upper, x.lower);    
+}
+
+
+int main(int argc, char *argv[])
+{
+    auto x = DoubleDouble(3.0);
+    auto y = DoubleDouble(1.5, 2.5e-18);
+
+    print_doubledouble("x", x);
+    print_doubledouble("y", y);
+
+    auto z = x + y;
+
+    print_doubledouble("   x + y", z);
+
+    z = x - y;
+    print_doubledouble("   x - y", z);
+
+    z = -x + y;
+    print_doubledouble("  -x + y", z);
+
+    z = x * y;
+    print_doubledouble("   x * y", z);
+
+    z = x / y;
+    print_doubledouble("   x / y", z);
+
+    z = y / x;
+    print_doubledouble("   y / x", z);
+
+    z = DoubleDouble(1.0) / x;
+    print_doubledouble("   1 / x", z);
+
+    z = y.powi(3);
+    print_doubledouble("    y**3", z);
+
+    z = y.exp();
+    print_doubledouble("  exp(y)", z);
+
+    z = get_pi().exp();
+    print_doubledouble(" exp(pi)", z);
+
+    z = get_pi().log();
+    print_doubledouble(" log(pi)", z);
+
+    z = get_pi().sqrt();
+    print_doubledouble("sqrt(pi)", z);
+    return 0;
+}
---
+++ c++/doubledouble/doubledouble.h
@@ -0,0 +1,212 @@
+//
+// A double-double class.
+// This is an incomplete translation of the Python code "doubledouble.py"
+// by Juraj Sukop, https://github.com/sukop/doubledouble
+// That file has the following license and copyright notice:
+// # Copyright (c) 2017, Juraj Sukop
+// #
+// # Permission to use, copy, modify, and/or distribute this software for any
+// # purpose with or without fee is hereby granted, provided that the above
+// # copyright notice and this permission notice appear in all copies.
+// #
+// # THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+// # REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+// # AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+// # INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// # LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+// # OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// # PERFORMANCE OF THIS SOFTWARE.
+//
+
+#ifndef DOUBLEDOUBLE_H
+#define DOUBLEDOUBLE_H
+
+#include <cstdlib>
+#include <cmath>
+#include <cfloat>
+
+
+class DoubleDouble
+{
+public:
+
+    double upper{0.0};
+    double lower{0.0};
+
+    constexpr
+    DoubleDouble() {}
+
+    constexpr
+    DoubleDouble(double upper, double lower) : upper(upper), lower(lower)
+    {}
+
+    constexpr
+    DoubleDouble(double upper) : upper(upper)
+    {}
+
+    DoubleDouble operator-();
+    DoubleDouble operator+(const DoubleDouble& x);
+    DoubleDouble operator-(const DoubleDouble& x);
+    DoubleDouble operator*(const DoubleDouble& x);
+    DoubleDouble operator/(const DoubleDouble& x);
+
+    DoubleDouble powi(int n);
+    DoubleDouble exp();
+    DoubleDouble log();
+    DoubleDouble sqrt();
+};
+
+
+constexpr
+DoubleDouble get_e();
+
+constexpr
+DoubleDouble get_pi();
+
+
+DoubleDouble two_sum_quick(double x, double y)
+{
+    double r = x + y;
+    double e = y - (r - x);
+    return DoubleDouble(r, e);
+}
+
+
+DoubleDouble two_sum(double x, double y)
+{
+    double r = x + y;
+    double t = r - x;
+    double e = (x - (r - t)) + (y - t);
+    return DoubleDouble(r, e);
+}
+
+
+DoubleDouble two_difference(double x, double y)
+{
+    double r = x - y;
+    double t = r - x;
+    double e = (x - (r - t)) - (y + t);
+    return DoubleDouble(r, e);
+}
+
+
+DoubleDouble two_product(double x, double y)
+{
+    double r = x*y;
+    double e = fma(x, y, -r);
+    return DoubleDouble(r, e);
+}
+
+
+DoubleDouble DoubleDouble::operator-()
+{
+    return DoubleDouble(-upper, -lower);
+}
+
+
+DoubleDouble DoubleDouble::operator+(const DoubleDouble& x)
+{
+    DoubleDouble re = two_sum(upper, x.upper);
+    re.lower += lower + x.lower;
+    return two_sum_quick(re.upper, re.lower);
+}
+
+
+DoubleDouble DoubleDouble::operator-(const DoubleDouble& x)
+{
+    DoubleDouble re = two_difference(upper, x.upper);
+    re.lower += lower - x.lower;
+    return two_sum_quick(re.upper, re.lower);
+}
+
+
+DoubleDouble DoubleDouble::operator*(const DoubleDouble& x)
+{
+    DoubleDouble re = two_product(upper, x.upper);
+    re.lower += upper*x.lower + lower*x.upper;
+    return two_sum_quick(re.upper, re.lower);
+}
+
+
+DoubleDouble DoubleDouble::operator/(const DoubleDouble& x)
+{
+    double r = upper/x.upper;
+    DoubleDouble sf = two_product(r, x.upper);
+    double e = (upper - sf.upper - sf.lower + lower - r*x.lower)/x.upper;
+    return two_sum_quick(r, e);
+}
+
+
+DoubleDouble DoubleDouble::powi(int n)
+{
+    int i = abs(n);
+    DoubleDouble b = *this;
+    DoubleDouble r(1);
+    while (1) {
+        if ((i & 1) == 1) {
+            r = r * b;
+        }
+        if (i <= 1) {
+            break;
+        }
+        i >>= 1;
+        b = b*b;
+    }
+    if (n < 0) {
+        return DoubleDouble(1) / r;
+    }
+    return r;
+}
+
+
+DoubleDouble DoubleDouble::exp()
+{
+    int n = int(round(upper));
+    DoubleDouble x(upper - n, lower);
+    DoubleDouble u = (((((((((((x +
+                     156)*x + 12012)*x +
+                     600600)*x + 21621600)*x +
+                     588107520)*x + 12350257920)*x +
+                     201132771840)*x + 2514159648000)*x +
+                     23465490048000)*x + 154872234316800)*x +
+                     647647525324800)*x + 1295295050649600;
+    DoubleDouble v = (((((((((((x -
+                     156)*x + 12012)*x -
+                     600600)*x + 21621600)*x -
+                     588107520)*x + 12350257920)*x -
+                     201132771840)*x + 2514159648000)*x -
+                     23465490048000)*x + 154872234316800)*x -
+                     647647525324800)*x + 1295295050649600;
+    return get_e().powi(n) * (u / v);
+}
+
+DoubleDouble DoubleDouble::sqrt()
+{
+    double r = std::sqrt(upper);
+    DoubleDouble sf = two_product(r, r);
+    double e = (upper - sf.upper - sf.lower + lower) * 0.5 / r;
+    return two_sum_quick(r, e);
+}
+
+DoubleDouble DoubleDouble::log()
+{
+    DoubleDouble r(std::log(upper));
+    DoubleDouble u = r.exp();
+    r = r - DoubleDouble(2.0)*(u - *this)/(u + *this);
+    return r;
+}
+
+constexpr
+DoubleDouble get_e()
+{
+    return DoubleDouble(0x1.5bf0a8b145769p+1, 1.44564689172925013472e-16 );
+}
+
+
+constexpr
+DoubleDouble get_pi()
+{
+    return DoubleDouble(0x1.921fb54442d18p+1, 1.22464679914735317636e-16);
+}
+
+#endif
