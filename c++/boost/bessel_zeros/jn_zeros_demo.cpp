d96b2f0278475a8e3e58c5e88094934af4d52a2e
Warren Weckesser <warren.weckesser@gmail.com>
2022-04-12 11:35:30 -0400

    c++/boost: Include zeros of Y in the Bessel zeros demo.

--- c++/boost/bessel_zeros/jn_zeros_demo.cpp
+++ c++/boost/bessel_zeros/jn_zeros_demo.cpp
@@ -6,11 +6,23 @@ using namespace boost::math;
 
 int main(int argc, char *argv[])
 {
-    double nu = 281;
+    double nu = 2;
 
+    std::cout << "Zeros of J_nu(x)" << std::endl;
+    std::cout << "  nu k x" << std::endl;
     for (int m = 1; m <= 6; ++m) {
        double z = cyl_bessel_j_zero(nu, m);
-       std::cout << std::scientific << std::setw(16)
+       std::cout << std::setw(4) << std::fixed << std::setprecision(1) << nu << " " << m << " "
+                 << std::scientific << std::setw(16)
+                 << std::setprecision(12) << z << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << "Zeros of Y_nu(x)" << std::endl;
+    std::cout << "  nu k x" << std::endl;
+    for (int m = 1; m <= 6; ++m) {
+       double z = cyl_neumann_zero(nu, m);
+       std::cout << std::setw(4) << std::fixed << std::setprecision(1) << nu << " " << m << " "
+                 << std::scientific << std::setw(16)
                  << std::setprecision(12) << z << std::endl;
     }
     return 0;
