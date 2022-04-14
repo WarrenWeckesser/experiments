22809135a588a139e6f33eaec4d97707d36f3b16
Warren Weckesser <warren.weckesser@gmail.com>
2022-04-14 11:54:30 -0400

    c++/boost: update erfinv_bad_input.cpp

--- c++/boost/erfinv/erfinv_bad_input.cpp
+++ c++/boost/erfinv/erfinv_bad_input.cpp
@@ -11,17 +11,18 @@ using boost::math::erf_inv;
 
 int main()
 {
-    double pvals[] = {-1.0, 1.0};
+    double pvals[] = {-3.5, -1.0, -0.25, 0, 0.96, 1.0, 2.3};
 
-    cout << "  p       erf_inv(p)" << endl;
+    cout << "  p         erf_inv(p)" << endl;
     for (const auto &p : pvals) {
-        double x = -999.25;
+        cout << scientific << setprecision(1) << setw(9) << p;
         try {
-            x = erf_inv(p);
+            double x = erf_inv(p);
+            cout << "   " << setprecision(17) << setw(24) << x << endl;
         } catch (const exception& e) {
+            cout << "   ***" << endl;
             cerr << "*** Caught: " << e.what() << endl;
             cerr << "*** Type: " << typeid(e).name() << endl;
         }
-        cout << scientific << setprecision(1) << p << " " << setprecision(17) << x << endl;
     }
 }
