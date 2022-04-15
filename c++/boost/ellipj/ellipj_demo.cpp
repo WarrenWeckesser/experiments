375a7354493ab640b1fa1eef1517cb729ad53533
warren <warren.weckesser@gmail.com>
2022-04-15 16:15:41 -0400

    c++/boost: update elliptic_jacobi example

--- c++/boost/ellipj/ellipj_demo.cpp
+++ c++/boost/ellipj/ellipj_demo.cpp
@@ -3,19 +3,21 @@
 #include <boost/math/special_functions/jacobi_elliptic.hpp>
 
 using namespace boost::math;
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    double m = 0.99999999997;
+    double k = 0.99999999997;
     double u = 50.0;
     double sn, cn, dn;
 
-    sn = jacobi_elliptic(m, u, &cn, &dn);
-    std::cout << std::scientific << std::setw(16)
-        << std::setprecision(12) << sn << std::endl;
-    std::cout << std::scientific << std::setw(16)
-        << std::setprecision(12) << cn << std::endl;
-    std::cout << std::scientific << std::setw(16)
-        << std::setprecision(12) << dn << std::endl;
+    sn = jacobi_elliptic(k, u, &cn, &dn);
+    cout << setprecision(17);
+    cout << "k = " << k << endl;
+    cout << "m = k**2 = " << k*k << endl;
+    cout << "u = " << u << endl;
+    cout << "sn = " << scientific << setw(17) << sn << endl;
+    cout << "cn = " << scientific << setw(17) << cn << endl;
+    cout << "dn = " << scientific << setw(17) << dn << endl;
     return 0;
 }
