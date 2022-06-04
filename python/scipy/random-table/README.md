41808b5ac0f2d854f231ff73767fe44a64295b46
Warren Weckesser <warren.weckesser@gmail.com>
2022-06-04 16:59:12 -0400

    python/scipy: update random_table.py

--- python/scipy/random-table/README.md
+++ python/scipy/random-table/README.md
@@ -1,7 +1,17 @@
-The Python function `random_table(table, size=None, rng=None)` generates a
-random contingency table with the same variable sums as the given table.
+The Python module `random_table.py` defines the functions `random_table()`
+and `random_table_from_table()`.  Both generate random contingency tables
+using the same underlying method.  The only difference is in the inputs
+to the functions.
 
-*Example*
+The Python function `random_table_from_table(table, size=None, rng=None)`
+generates a random contingency table with the same variable sums as the
+given table.
+
+The function `random_table(*sums, size=None, rng=None)` accepts the
+quantities associated with the levels of each categorical variable
+(i.e. the marginal sums for each variable).
+
+*Examples*
 
 ```
 In [71]: c = np.array([[36, 5, 14], [14, 10, 21]])
@@ -15,7 +25,7 @@ array([[36,  5, 14],
 Generate one random table:
 
 ```
-In [73]: random_table(c)
+In [73]: random_table_from_table(c)
 Out[73]: 
 array([[28,  6, 21],
        [22,  9, 14]])
@@ -26,7 +36,7 @@ The `size` parameter allows several random tables to be generated in one call:
 
 ```
 
-In [74]: random_table(c, size=3)
+In [74]: random_table_from_table(c, size=3)
 Out[74]: 
 array([[[28,  7, 20],
         [22,  8, 15]],
@@ -42,7 +52,7 @@ Generate a large number of tables, and compare the sample mean to the expected
 mean computed by `scipy.stats.contingency.expected_freq`:
 
 ```
-In [75]: tables = random_table(c, size=100000)
+In [75]: tables = random_table_from_table(c, size=100000)
 
 In [76]: tables.mean(axis=0)
 Out[76]: 
@@ -89,7 +99,7 @@ Out[88]:
 ```
 Generate a random table from `c3`.
 ```
-In [89]: sample = random_table(c3)
+In [89]: sample = random_table_from_table(c3)
 
 In [90]: sample
 Out[90]:
@@ -112,4 +122,41 @@ Out[91]:
          [ 90],
          [ 54]]]),
  array([[[125,  70,  55]]])]
+```
+
+Instead of an existing contingency table, the function `random_table()`
+accepts the marginal sums associated with each categorical variable.
+For example,
+
+```
+In [94]: random_table([10, 10, 20], [15, 25])
+Out[94]:
+array([[ 3,  7],
+       [ 4,  6],
+       [ 8, 12]])
+```
+Note that the sums of the rows are [10, 10, 20], and the sums of
+the columns are [15, 25].
+
+Higher-dimensional tables can also be generated with this function.
+The following generates one contingency table with shape (3, 2, 3):
+```
+In [95]: table = random_table([10, 10, 20], [15, 25], [6, 28, 6])
+
+In [96]: table
+Out[96]:
+array([[[0, 3, 0],
+        [0, 6, 1]],
+
+       [[1, 3, 0],
+        [0, 5, 1]],
+
+       [[2, 4, 2],
+        [3, 7, 2]]])
+```
+Verify that the margins have the required sums.
+
+```
+In [97]: [t.ravel() for t in margins(table)]
+Out[97]: [array([10, 10, 20]), array([15, 25]), array([ 6, 28,  6])]
 ```
--- python/scipy/random-table/random_table.py
+++ python/scipy/random-table/random_table.py
@@ -7,6 +7,9 @@ def _fast_2d_crosstab(x, y, mx, my):
     """
     Fast crosstab for nonnegative integer arrays x and y.
     The function assumes x < mx and y < my (note the inequalities are strict).
+
+    "fast" just means it is faster than the general-purpose function
+    scipy.stats.contingency.crosstab.
     """
     z = my*x + y
     return np.bincount(z, minlength=mx*my).reshape((mx, my))
@@ -14,9 +17,9 @@ def _fast_2d_crosstab(x, y, mx, my):
 
 def _random_2d_table_from_marginals(marginal0, marginal1, size=None, rng=None):
     """
-    Inputs are marginal sums associated with axis 0 and 1, resp.
-    E.g. if c is the table, then marginal0 is c.sum(axis=0)
-    and marginal1 is c.sum(axis=1).
+    Inputs are marginal sums associated with the two categorical
+    variables.  E.g. if c is the table, then marginal0 is c.sum(axis=1)
+    and marginal1 is c.sum(axis=0).
 
     This is a slow way to generate random 2-d contingency tables.
     """
@@ -34,13 +37,13 @@ def _random_2d_table_from_marginals(marginal0, marginal1, size=None, rng=None):
 
     len0 = len(marginal0)
     len1 = len(marginal1)
-    x = np.repeat(np.arange(len1), marginal1)
-    y = np.repeat(np.arange(len0), marginal0)
+    x = np.repeat(np.arange(len0), marginal0)
+    y = np.repeat(np.arange(len1), marginal1)
 
-    tables = np.empty((nvars, len1, len0), dtype=int)
+    tables = np.empty((nvars, len0, len1), dtype=int)
     for k in range(nvars):
         rng.shuffle(x)
-        tables[k] = _fast_2d_crosstab(x, y, len1, len0)
+        tables[k] = _fast_2d_crosstab(x, y, len0, len1)
     if size is None:
         tables = tables[0]
     return np.array(tables)
@@ -57,8 +60,8 @@ def _random_2d_table(table, size=None, rng=None):
     """
     if table.ndim != 2:
         raise ValueError('table must be a 2-d array')
-    m0 = table.sum(axis=0)
-    m1 = table.sum(axis=1)
+    m0 = table.sum(axis=1)
+    m1 = table.sum(axis=0)
     return _random_2d_table_from_marginals(m0, m1, size=size, rng=rng)
 
 
@@ -75,7 +78,7 @@ def _fast_crosstab(data, maxes, maxprod, coeffs):
     return np.bincount(z, minlength=maxprod).reshape(maxes)
 
 
-def random_table(table, size=None, rng=None):
+def random_table_from_table(table, *, size=None, rng=None):
     """
     Generate a random contingency table.
 
@@ -86,7 +89,7 @@ def random_table(table, size=None, rng=None):
     an array with shape ``(size,) + table.shape`` is returned, containing
     ``size`` random tables.
 
-    The method used to generate the random table is simple but *slow*.
+    The method used to generate the random table is simple but slow.
     Let ``m`` be the number of dimensions of ``table``, and let ``N``
     be the sum of the elements in the table.  Both the time and space
     complexities of the algorithm to generate one sample are O(``m*N``).
@@ -96,10 +99,90 @@ def random_table(table, size=None, rng=None):
     """
     table = np.asarray(table)
     ndim = table.ndim
+    axes = set(range(ndim))
+    sums = []
+    for k in range(ndim):
+        a = tuple(axes - {k})
+        # m is the sum over all axes except k.
+        m = table.sum(axis=a)
+        sums.append(m)
+    return random_table(*sums, size=size, rng=rng)
+
+
+def random_table(*sums, size=None, rng=None):
+    """
+    Generate a random contingency table.
+
+    Given the sums of the levels of a set of categorical variables,
+    generate a random contingency table that is consistent with those
+    sums.
+
+    * The number of categorical variables (and therefore the number
+      of dimensions of the table) is ``len(sums)``.
+    * The number of levels for categorical variable ``k`` is
+      ``len(sums[k])``, so the shape of a random table will be
+      ``tuple(len(s) for s in sums)``.
+    * ``sums[k]`` gives the number of occurrences of each level for
+      for categorical variable ``k``.  Another way to say this is that
+      ``sums[k]`` is the marginal sum of the table over all axes except
+      ``k``.
+
+    By default, one random table is returned.  If ``size`` is an integer,
+    an array with shape ``(size,) + table.shape`` is returned, containing
+    ``size`` random tables.
+
+    The method used to generate the random table is simple but slow. Let
+    ``m`` be the number of variables (i.e. ``m = len(sums)``), and let
+    ``N`` be the sum of the elements in the table (so ``N = sum(sums[0])``).
+    Both the time and space complexities of the algorithm to generate one
+    random table are O(``m*N``).
+
+    There is a lot of literature on the efficient generation of random
+    contingency tables that has been completely ignored here!
+
+    Examples
+    --------
+    Among a group of 100 people, 87 are right-handed and 13 are
+    left-handed.  Also, when asked about their favorite ice cream,
+    34 said chocolate, 23 said vanilla, 13 said strawberry, 15 said
+    some other flavor and 15 had no preference.
+
+    There are two categorical variables: *handedness* and *flavor
+    preference*.  Handedness has two levels: *right* and *left*.  Flavor
+    preference has five levels: *chocolate*, *vanilla*, *strawberry*,
+    *other* and *none*.  The sums for these variables are [87, 13] and
+    [34, 23, 13, 15, 15], respectively.  To generate a random contingency
+    table consistent with this data, we can do the following:
+
+    >>> random_table([87, 13], [34, 23, 13, 15, 15])
+    array([[28, 18, 13, 13, 15],
+           [ 6,  5,  0,  2,  0]])
+
+    To generate three such tables with one call of ``random_table``:
+
+    >>> random_table([87, 13], [34, 23, 13, 15, 15], size=3)
+    array([[[31, 21, 12, 10, 13],
+            [ 3,  2,  1,  5,  2]],
+
+           [[28, 20, 11, 14, 14],
+            [ 6,  3,  2,  1,  1]],
+
+           [[33, 20, 11, 10, 13],
+            [ 1,  3,  2,  5,  2]]])
+
+    """
+    sums = [np.asarray(s) for s in sums]
+    N = sums[0].sum()
+    for s in sums[1:]:
+        if s.sum() != N:
+            raise ValueError('All the input sequences must have the same sum.')
+
+    ndim = len(sums)
     if ndim == 2:
-        # The 2-d specialization is a bit faster than the
-        # generic n-d code in this function.
-        return _random_2d_table(table, size=size, rng=rng)
+        return _random_2d_table_from_marginals(sums[0], sums[1],
+                                               size=size, rng=rng)
+
+    table_shape = tuple(len(s) for s in sums)
 
     if rng is None:
         rng = np.random.default_rng()
@@ -119,15 +202,7 @@ def random_table(table, size=None, rng=None):
     else:
         nvars = size
 
-    axes = set(range(ndim))
-    sums = []
-    for k in range(ndim):
-        a = tuple(axes - {k})
-        # m is the sum over all axes except k.
-        m = table.sum(axis=a)
-        sums.append(m)
-
-    data = np.empty((ndim, table.sum()), dtype=int)
+    data = np.empty((ndim, N), dtype=int)
     for k, s in enumerate(sums):
         data[k] = np.repeat(np.arange(len(s)), s)
 
@@ -138,10 +213,12 @@ def random_table(table, size=None, rng=None):
     maxprod = cm[-1]
     coeffs = np.r_[[1], cm[:-1]][::-1]
 
-    tables = np.empty((nvars,) + table.shape, dtype=int)
+    tables = np.empty((nvars,) + table_shape, dtype=int)
     for k in range(nvars):
         rowshuffle(data[:-1])
         tables[k] = _fast_crosstab(data, maxes, maxprod, coeffs)
+
     if size is None:
         tables = tables[0]
+
     return tables
