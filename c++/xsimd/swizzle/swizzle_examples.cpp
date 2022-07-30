54d73fc0749235f0dfe28c69194f4e6492a6d803
warren <warren.weckesser@gmail.com>
2022-07-30 03:26:30 -0400

    c++/xsimd: Update swizzle examples.

--- c++/xsimd/swizzle/swizzle_examples.cpp
+++ c++/xsimd/swizzle/swizzle_examples.cpp
@@ -6,8 +6,7 @@
 namespace xs = xsimd;
 
 //
-// Experimenting with swizzle and performing a min reduction of a batch.
-// Currently the batches used here are hardcoded avx float (8 lanes).
+// Experimenting with swizzle.
 //
 
 //
@@ -18,7 +17,7 @@ namespace xs = xsimd;
 //
 struct ShuffleLowHigh
 {
-    static constexpr uint32_t get(size_t index, size_t size)
+    static constexpr size_t get(size_t index, size_t size)
     {
         return (index & 1) ? size/2 + (index - 1)/2 : index/2;
     }
@@ -31,73 +30,69 @@ struct ShuffleLowHigh
 //
 struct SwapPairs
 {
-    static constexpr uint32_t get(size_t index, size_t size)
+    static constexpr size_t get(size_t index, size_t size)
     {
         return index ^ 1;
     }
 };
 
 
-//
-// ShiftLeft swizzle pattern generator.
-//
-template<typename T, size_t n>
+template<size_t n>
 struct ShiftLeft
 {
-    static constexpr T get(size_t index, size_t size)
+    static constexpr size_t get(size_t index, size_t size)
     {
         return (index + n) % size;
     }
 };
 
 
+template <class T>
+struct as_index
+{
+    using type = xs::as_unsigned_integer_t<T>;
+};
+
+
 int main(int argc, char* argv[])
 {
-    xs::batch<float, xs::avx> a = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0};
+    using BatchType = xs::batch<float, xs::avx>;
+    using IndexType = typename as_index<BatchType>::type;
+
+    BatchType a = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0};
+
     std::cout << "a:  " << a << std::endl;
 
-    constexpr auto shuffle_low_high = xs::make_batch_constant<xs::batch<std::uint32_t,
-                                                                        xs::avx>,
+    constexpr auto shuffle_low_high = xs::make_batch_constant<IndexType,
                                                               ShuffleLowHigh>();
 
     auto b = xs::swizzle(a, shuffle_low_high);
     std::cout << "b:  " << b << " ShuffleLowHigh" << std::endl;
 
-    constexpr auto swap_pairs = xs::make_batch_constant<xs::batch<std::uint32_t,
-                                                                  xs::avx>,
+    constexpr auto swap_pairs = xs::make_batch_constant<IndexType,
                                                         SwapPairs>();
     auto c = xs::swizzle(a, swap_pairs);
     std::cout << "c:  " << c << " SwapPairs" << std::endl;
 
-    constexpr auto shift1 = xs::make_batch_constant<xs::batch<std::uint32_t, xs::avx>,
-                                                    ShiftLeft<uint32_t, 1>>();
-    constexpr auto shift2 = xs::make_batch_constant<xs::batch<std::uint32_t, xs::avx>,
-                                                    ShiftLeft<uint32_t, 2>>();
-    constexpr auto shift4 = xs::make_batch_constant<xs::batch<std::uint32_t, xs::avx>,
-                                                    ShiftLeft<uint32_t, 4>>();
+    constexpr auto shift1 = xs::make_batch_constant<IndexType,
+                                                    ShiftLeft<1>>();
+    constexpr auto shift2 = xs::make_batch_constant<IndexType,
+                                                    ShiftLeft<2>>();
+
+
     auto s1 = xs::swizzle(a, shift1);
     auto s2 = xs::swizzle(a, shift2);
-    auto s4 = xs::swizzle(a, shift4);
-    std::cout << "s1: " << s1 << " ShiftLeft<uint32_t, 1>" << std::endl;
-    std::cout << "s2: " << s2 << " ShiftLeft<uint32_t, 2>" << std::endl;
-    std::cout << "s4: " << s4 << " ShiftLeft<uint32_t, 4>" << std::endl;
+    std::cout << "s1: " << s1 << " ShiftLeft<1>" << std::endl;
+    std::cout << "s2: " << s2 << " ShiftLeft<2>" << std::endl;
+
+    if (a.size > 4) {
+        constexpr auto shift4 = xs::make_batch_constant<IndexType,
+                                                        ShiftLeft<4>>();
+        auto s4 = xs::swizzle(a, shift4);
+        std::cout << "s4: " << s4 << " ShiftLeft<4>" << std::endl;
+    }
 
     std::cout << std::endl;
-
-    xs::batch<float, xs::avx> x = {3.0, 1.0, -2.0, 2.5, -1.0, 4.0, 3.5, 2.0};
-    std::cout << "x: " << x << std::endl;
-
-    // Use shift4, shift2 and shift1 to compute the min reduction of the
-    // 8 lane batch x.
-    auto y = xs::min(x, xs::swizzle(x, shift4));
-    y = xs::min(y, xs::swizzle(y, shift2));
-    y = xs::min(y, xs::swizzle(y, shift1));
-    
-    std::cout << "y: " << y << std::endl;
-
-    float xbuf[x.size];
-    y.store_unaligned(xbuf);
-    std::cout << "min(x) = " << xbuf[0] << std::endl;
 
     return 0;
 }
