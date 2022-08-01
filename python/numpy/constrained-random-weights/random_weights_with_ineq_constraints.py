e02081aaa759673896f77d338dd2010b357d63cd
warren <warren.weckesser@gmail.com>
2022-08-01 14:17:51 -0400

    pyhton/numpy: Update the 'random weights' function.

--- python/numpy/constrained-random-weights/random_weights_with_ineq_constraints.py
+++ python/numpy/constrained-random-weights/random_weights_with_ineq_constraints.py
@@ -55,7 +55,7 @@ def compute_intersection_vertices(bounds):
     return np.array(constrained_points)
 
 
-def random_weights(bounds, nsamples, rng=None):
+def _random_weights_rejection(bounds, nsamples, cmin, cmax, rng):
     """
     Generate random weights.
 
@@ -67,7 +67,27 @@ def random_weights(bounds, nsamples, rng=None):
 
     This is a rejection method.  ``nsamples`` is the number of *candidates*
     to be generated.  The number of samples returned will generally be
-    less than ``nsamples``.  Be careful: the method implemented by this
+    less than ``nsamples``.
+    """
+    s = rng.uniform(cmin[:-1], cmax[:-1], size=(nsamples, len(bounds)-1))
+    z = 1 - s.sum(axis=1)
+    mask = (bounds[-1, 0] < z) & (z < bounds[-1, 1])
+
+    samples = np.column_stack((s[mask], z[mask]))
+    return samples
+
+
+def random_weights(bounds, nsamples, rng=None):
+    """
+    Generate random weights.
+
+    A "weights" sample is a 1-d array of values in [0, 1] that sum to 1.
+
+    For this function, the length of the array is set by ``len(bounds)``.
+    ``bounds`` must be a 2-d NumPy array with shape ``(n, 2)``. Each
+    row gives the lower and upper bound of the corresponding random weight.
+
+    This is a rejection method.  Be careful: the method implemented by this
     function has no lower bound on the acceptance rate; for some ``bounds``,
     the acceptance rate can be *very* low.
 
@@ -75,19 +95,24 @@ def random_weights(bounds, nsamples, rng=None):
     --------
     >>> import numpy as np
     >>> rng = np.random.default_rng(889834848603455)
-    >>> bounds = np.array([[0.1, 0.2], [0.2, 0.8], [0.05, 0.2], [0.05, 0.1]])
+    >>> bounds = np.array([[0.1, 0.3], [0.2, 0.8], [0.05, 0.2], [0.05, 0.2]])
     >>> w = random_weights(bounds, 10, rng=rng)
     >>> w
-    array([[0.1716904 , 0.60920283, 0.13659622, 0.08251055],
-           [0.17366032, 0.64377311, 0.08603213, 0.09653444],
-           [0.18063014, 0.59238271, 0.13229396, 0.0946932 ]])
-
-    Note that 10 samples were requested but only 3 were generated.
+    array([[0.2433808 , 0.48200472, 0.13659622, 0.13801826],
+           [0.24732063, 0.53962185, 0.08603213, 0.12702538],
+           [0.26126027, 0.45397118, 0.13229396, 0.15247459],
+           [0.12556574, 0.64381902, 0.05610864, 0.17450659],
+           [0.23442762, 0.48725061, 0.10074363, 0.17757814],
+           [0.14484602, 0.61525839, 0.05651471, 0.18338088],
+           [0.11788144, 0.69038204, 0.12077772, 0.07095881],
+           [0.21745906, 0.53443103, 0.0702289 , 0.17788101],
+           [0.23175572, 0.6135676 , 0.08310453, 0.07157215],
+           [0.11296737, 0.65230007, 0.15007042, 0.08466214]])
 
     Check that the rows sum to 1:
 
     >>> w.sum(axis=1)
-    array([1., 1., 1.])
+    array([1., 1., 1., 1., 1., 1., 1., 1., 1., 1.])
 
     """
     if rng is None:
@@ -97,9 +122,12 @@ def random_weights(bounds, nsamples, rng=None):
     cmin = constrained_points.min(axis=0)
     cmax = constrained_points.max(axis=0)
 
-    s = rng.uniform(cmin[:-1], cmax[:-1], size=(nsamples, bounds.shape[0]-1))
-    z = 1 - s.sum(axis=1)
-    mask = (bounds[-1, 0] < z) & (z < bounds[-1, 1])
-
-    samples = np.column_stack((s[mask], z[mask]))
+    samples = np.zeros((nsamples, len(bounds)))
+    nfilled = 0
+    while nfilled < nsamples:
+        sample1 = _random_weights_rejection(bounds, nsamples - nfilled,
+                                            cmin, cmax, rng)
+        m = len(sample1)
+        samples[nfilled:nfilled+m, :] = sample1
+        nfilled += m
     return samples
