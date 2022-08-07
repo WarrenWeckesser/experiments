ef29c86a08d484646ec4a34e8b862268a5db011d
Warren Weckesser <warren.weckesser@gmail.com>
2022-08-07 11:52:35 -0400

    c/simd: Fix use of n in cabs2f_sse

--- c/simd/cabs2.c
+++ c/simd/cabs2.c
@@ -50,9 +50,10 @@ int cabs2f(size_t n, const float *z, float *out)
 
 int cabs2f_sse(size_t n, const float *z, float *out)
 {
-    size_t r = n % 4;
+    size_t m = 2*n;
+    size_t r = m % 4;
     size_t k = 0;
-    for (; k < n - r; k += 4) {
+    for (; k < m - r; k += 4) {
         __m128 z4 = _mm_loadu_ps(z + k);
         //print_m128("z4:  ", z4);
         __m128 s1 = _mm_mul_ps(z4, z4);
@@ -63,8 +64,10 @@ int cabs2f_sse(size_t n, const float *z, float *out)
         //print_m128("ss:  ", ss);
         __m128 as2 = _mm_shuffle_ps(ss, ss, _MM_SHUFFLE(1,3,2,0));
         //print_m128("as2: ", as2);
-        _mm_store_sd((double *) (out+k/2), (__m128d) as2);
+        _mm_store_sd((double *) (out+k/2), _mm_castps_pd(as2));
     }
-    cabs2f(r, z + k, out + k/2);
+    if (r > 0) {
+        cabs2f(r/2, z + k, out + k/2);
+    }
     return 0;
 }
--- c/simd/cabs2_demo.c
+++ c/simd/cabs2_demo.c
@@ -15,39 +15,40 @@ void print_float_array(size_t n, float *a)
 
 int main(int argc, char *argv[])
 {
-    size_t n = argc - 1;
-    if (n < 2) {
+    size_t m = argc - 1;
+    if (m < 2) {
         fprintf(stderr, "At least two numbers are required on the command line.\n");
         exit(-1);
     }
-    if (n & 1) {
+    if (m & 1) {
         fprintf(stderr, "The number of values must be even.  The last value is will be ignored.\n");
-        n -= 1;
+        m -= 1;
     }
-    float *mem = malloc((n + n/2) * sizeof(float));
+    size_t n = m / 2;
+    float *mem = malloc((3*n) * sizeof(float));
     if (mem == NULL) {
         fprintf(stderr, "malloc failed\n");
         exit(-1);
     }
     float *x = mem;
-    float *out = mem + n;
+    float *out = mem + m;
 
-    for (size_t i = 0; i < n; ++i) {
+    for (size_t i = 0; i < m; ++i) {
         x[i] = strtof(argv[i+1], NULL);
     }
-    for (size_t i = 0; i < n; i += 2) {
+    for (size_t i = 0; i < m; i += 2) {
         printf("z[%ld] = %8.3f + %8.3fi\n", i/2, x[i], x[i+1]);
     }
 
     cabs2f(n, x, out);
-    print_float_array(n/2, out);
+    print_float_array(n, out);
 
-    for (size_t k = 0; k < n/2; ++k) {
+    for (size_t k = 0; k < n; ++k) {
         out[k] = 0.0f;
     }
 
     cabs2f_sse(n, x, out);
-    print_float_array(n/2, out);
+    print_float_array(n, out);
 
     return 0;
 }
