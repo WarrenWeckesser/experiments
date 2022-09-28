958dcdbb47b89ffeaae4ca872cc69ed8ed54bd04
Warren Weckesser <warren.weckesser@gmail.com>
2022-09-28 07:28:41 -0400

    python/scipy: Update colebrook code: license, use 3.71 by default instead of 3.7

--- python/scipy/colebrook/colebrook.py
+++ python/scipy/colebrook/colebrook.py
@@ -1,10 +1,34 @@
+# Copyright (c) 2022, Warren Weckesser
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 import math
 import numpy as np
 from scipy.special import lambertw
 
 
-def colebrook(Re, Dh, eps):
+def colebrook(Re, Dh, eps, constant1=3.71):
     """
     Solve the Colebrook equation for f, given Re, D and eps.
 
@@ -24,6 +48,10 @@ def colebrook(Re, Dh, eps):
 
             1/sqrt(f) = -2*log(eps/(3.7*Dh) + 2.51/(Re*sqrt(f))
 
+    Many references use the constant 3.71 instead of 3.7, and this
+    function does, too.  If one needs to used 3.7, specify that value as
+    the parameter `constant1`.
+
     Parameters
     ----------
     Re : array_like
@@ -33,6 +61,9 @@ def colebrook(Re, Dh, eps):
         conduits, Dh = D = inside diameter."  Must have the same units as eps.
     eps : array_like
         Absolute pipe roughness.  Must have the same units as Dh.
+    constant1 : float, optional
+        This option allows the constant 3.71 in the implicit equation
+        to be replaced with some other values (e.g. 3.7).
 
     Returns
     -------
@@ -47,14 +78,14 @@ def colebrook(Re, Dh, eps):
     Examples
     --------
     >>> colebrook(Re=5e6, Dh=1250.0, eps=[0.01, 0.02, 0.04, 0.08])
-    array([0.00947893, 0.00988838, 0.01054755, 0.0115219 ])
+    array([0.00947772, 0.00988635, 0.01054441, 0.01151743])
 
     """
     Re = np.asarray(Re)
     Dh = np.asarray(Dh)
     eps = np.asarray(eps)
     a = 2.51 / Re
-    b = eps / (3.7*Dh)
+    b = eps / (constant1*Dh)
     p = 1/math.sqrt(10)
     lnp = math.log(p)
     x = -lambertw(-lnp/a * np.power(p, -b/a))/lnp - b/a
