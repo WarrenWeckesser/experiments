bc359c31db3781505098a46fa8e6cf2140b929fb
warren <warren.weckesser@gmail.com>
2022-09-30 12:42:24 -0400

    boost/c++: Accept values from command line in the demo program hyp1f1.cpp

--- c++/boost/hypergeometric_1f1/hyp1f1.cpp
+++ c++/boost/hypergeometric_1f1/hyp1f1.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <iostream>
 #include <iomanip>
 #include <boost/math/special_functions/hypergeometric_1F1.hpp>
@@ -17,12 +18,15 @@ int main(int argc, char *argv[])
 {
     double a, b, x, y;
 
-
-	a = 0.5;
-    b = 4.0;
-    x = 0.5;
+    if (argc != 4 ) {
+        printf("use: hyp1f1 a b x\n");
+        return -1;
+    }
+    a = std::stod(argv[1]);
+    b = std::stod(argv[2]);
+    x = std::stod(argv[3]);
     y = hypergeometric_1F1(a, b, x);
-	std::cout << std::scientific << std::setw(16)
+    std::cout << std::scientific << std::setw(16)
         << std::setprecision(12) << y << std::endl;
-	return 0;
+    return 0;
 }
