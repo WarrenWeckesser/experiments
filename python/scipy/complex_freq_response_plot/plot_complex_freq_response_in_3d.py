bac0eb8a0f650ce0496b3c5ce6820551278c4a2a
warren <warren.weckesser@gmail.com>
2022-10-26 10:56:18 -0400

    python/scipy: Include a regular frequency response plot in complex_freq_response_plot

--- python/scipy/complex_freq_response_plot/plot_complex_freq_response_in_3d.py
+++ python/scipy/complex_freq_response_plot/plot_complex_freq_response_in_3d.py
@@ -7,12 +7,21 @@ taps = remez(31, [0, 0.3, 0.4, 1], [1, 0], fs=2)
 
 w, h = freqz(taps, worN=500, fs=2)
 
+ax1 = plt.subplot()
+ax1.plot(w, np.abs(h))
+ax1.set_xlabel('frequency')
+ax1.set_ylabel('gain, abs(h)')
+ax1.grid(True)
+
+plt.savefig('plot_complex_freq_response_in_3d_fig1.svg')
+
 fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.plot(w, h.real, h.imag)
-ax.set_xlabel('frequency')
-ax.set_ylabel('Re(h)')
-ax.set_zlabel('Im(h)')
+ax2 = fig.add_subplot(projection='3d')
+ax2.plot(w, h.real, h.imag)
+ax2.set_xlabel('frequency')
+ax2.set_ylabel('Re(h)')
+ax2.set_zlabel('Im(h)')
+
+plt.savefig('plot_complex_freq_response_in_3d_fig2.svg')
 
 plt.show()
-# plt.savefig('plot_complex_freq_response_in_3d.svg')
