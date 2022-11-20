eb629ddf2eddf926503b905df465744f945ac20d
warren <warren.weckesser@gmail.com>
2022-11-20 05:33:49 -0500

    c++/boost: In hyp2f1 demo, accept args from the command line.

--- c++/boost/hypergeometric_2f1/hyp2f1.cpp
+++ c++/boost/hypergeometric_2f1/hyp2f1.cpp
@@ -13,15 +13,20 @@ double hyp2f1(double a1, double a2, double b1, double x)
 
 int main(int argc, char *argv[])
 {
-    double a1, a2, b1, x, y;
+    if (argc != 5) {
+        std::cout << "use: " << argv[0] << " a1 a2 b x\n";
+        return -1;
+    }
 
+    double a1 = std::stod(argv[1]);
+    double a2 = std::stod(argv[2]);
+    double b = std::stod(argv[3]);
+    double x = std::stod(argv[4]);
 
-	a1 = 0.5;
-    a2 = 1.5;
-    b1 = 4.0;
-    x = 0.5;
-    y = hyp2f1(a1, a2, b1, x);
-	std::cout << std::scientific << std::setw(16)
-        << std::setprecision(12) << y << std::endl;
-	return 0;
+    double y = hyp2f1(a1, a2, b, x);
+
+    std::cout << std::scientific << std::setw(24)
+        << std::setprecision(17) << y << std::endl;
+
+    return 0;
 }
