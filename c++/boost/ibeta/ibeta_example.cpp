6c0171985524fbc8878555e41f39def8b47eb98d
warren <warren.weckesser@gmail.com>
2022-11-20 05:38:54 -0500

    c++/boost: In ibeta example, accept args from the command line.

--- c++/boost/ibeta/ibeta_example.cpp
+++ c++/boost/ibeta/ibeta_example.cpp
@@ -6,13 +6,28 @@ using namespace boost::math;
 
 int main(int argc, char *argv[])
 {
-	double a, b, x, y;
+    if (argc != 4) {
+        std::cout << "use: " << argv[0] << " a b x\n";
+        return -1;
+    }
 
-	a = 0.5;
-	b = 5e-7;
-	x = 0.9999990000010001;
-	y = ibeta(a, b, x);
-	std::cout << std::scientific << std::setw(16)
-        << std::setprecision(12) << y << std::endl;
-	return 0;
+    double a = std::stod(argv[1]);
+    double b = std::stod(argv[2]);
+    double x = std::stod(argv[3]);
+
+    double y1 = ibeta(a, b, x);
+    std::cout << std::scientific << std::setw(24)
+        << std::setprecision(17) << y1 << std::endl;
+
+    double y1full = y1 * beta(a, b);
+    std::cout << std::scientific << std::setw(24)
+        << std::setprecision(17) << y1full << std::endl;
+
+    if (a > 0 && b > 0) {
+        double y2 = beta(a, b, x);
+        std::cout << std::scientific << std::setw(24)
+            << std::setprecision(17) << y2 << std::endl;
+    }
+
+    return 0;
 }
