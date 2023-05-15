ae7f29803d823e7dff48f209fd71c9b5b97b23bd
warren <warren.weckesser@gmail.com>
2023-05-15 00:46:20 -0400

    c: Add a short program that invokes undefined behavior of casting large floating point to int.

--- c/cast-large-double-to-int64/main.c
+++ c/cast-large-double-to-int64/main.c
@@ -3,6 +3,10 @@
 
 int main(int argc, char *argv[])
 {
+    // Casting a floating point value to an int when the value exceeds
+    // the maximum of the integer type is undefined behavior.  The result
+    // is compiler-dependent.
+
     double x = 9223372036854775808.0;
     int64_t i = (int64_t) x;
 
