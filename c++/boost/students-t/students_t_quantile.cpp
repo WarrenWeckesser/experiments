6c05e330a770a4a4c221dec9afade46e841623f6
warren <warren.weckesser@gmail.com>
2023-06-14 15:58:53 -0400

    c++/boost: add command-line demo for quantile function of Student's t distr.

---
+++ c++/boost/students-t/students_t_quantile.cpp
@@ -0,0 +1,36 @@
+
+
+#include <boost/math/distributions/students_t.hpp>
+   using boost::math::students_t;  // Probability of students_t(df, t).
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using std::cout;
+using std::endl;
+using std::setprecision;
+using std::setw;
+using std::sqrt;
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        std::cout << "use: " << argv[0] << " dof p\n";
+        return -1;
+    }
+    cout << "Student's t distribution, quantile function. " << endl;
+
+    double dof = std::stod(argv[1]);
+    double p = std::stod(argv[2]);
+
+    cout << "dof = " << dof << endl;
+    cout << "p = " << p << endl;
+
+    auto dist = students_t(dof);
+    double x = quantile(dist, p);
+
+    cout << "quantile: x = " << setw(18) << setprecision(15) << x << endl;
+
+    return 0;
+}
