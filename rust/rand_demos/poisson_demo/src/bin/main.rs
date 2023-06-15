53f72b0389f28808439fdc8ce335d6e14bf12849
warren <warren.weckesser@gmail.com>
2023-06-15 17:15:50 -0400

    rust/rand_demos: Tweak poisson demo.

--- rust/rand_demos/poisson_demo/src/bin/main.rs
+++ rust/rand_demos/poisson_demo/src/bin/main.rs
@@ -1,9 +1,12 @@
 use rand_distr::{Distribution, Poisson};
 
 fn main() {
-    let lambda = 1.0e306f64;
+    // let lambda = 1.0e306f64;
+    let lambda = 5.0e2f64;
     let poisson = Poisson::<f64>::new(lambda).unwrap();
     let mut rng = rand::thread_rng();
-    let k = poisson.sample(&mut rng);
-    println!("{}", k);
+    for _ in 0..8 {
+        let k = poisson.sample(&mut rng);
+        println!("{}", k);
+    }
 }
