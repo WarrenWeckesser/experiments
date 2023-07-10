3a0d8a87c3c0f50b230ae8d5a0f33730084f250f
warren <warren.weckesser@gmail.com>
2023-07-10 17:29:37 -0400

    c++/cln: Adjust whitespace in the demo.

--- c++/cln/benktander2_cdf_demo.cpp
+++ c++/cln/benktander2_cdf_demo.cpp
@@ -4,17 +4,14 @@
 
 using namespace cln;
 
-
 //
 // Cumulative distribution function of the Benktander type II probability
 // distribution.  See, for example,
 //     https://en.wikipedia.org/wiki/Benktander_type_II_distribution
 //
-const cl_F benktander2_cdf(const cl_F& x,
-                            const cl_F& a,
-                            const cl_F& b)
+const cl_F benktander2_cdf(const cl_F& x, const cl_F& a, const cl_F& b)
 {
-    return 1 - exp((b - 1)*ln(x) + (a / b)*(1 - exp(b * ln(x))));
+    return 1 - exp((b - 1)*ln(x) + (a/b)*(1 - exp(b*ln(x))));
 }
 
 int main(int argc, char *argv[])
