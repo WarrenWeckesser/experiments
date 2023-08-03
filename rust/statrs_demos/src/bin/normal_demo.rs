48855566c01b9111251ccbba38f8b37c2ed07ce1
warren <warren.weckesser@gmail.com>
2023-08-03 15:16:01 -0400

    rust/statrs: Tweak normal_demo.rs

--- rust/statrs_demos/src/bin/normal_demo.rs
+++ rust/statrs_demos/src/bin/normal_demo.rs
@@ -18,11 +18,20 @@ fn main() {
     let mu = 0.0;
     let sigma = 1.0;
     let normal = Normal::new(mu, sigma).unwrap();
-    let x0 = -35.0;
+    let x0 = 4.25;
+    print!("x0 = ");
+    out(x0, true);
     let p = normal.cdf(x0);
-    print!("normal.cdf(x0)=");
+    print!("normal.cdf(x0) = ");
     out(p, true);
     let x1 = normal.inverse_cdf(p);
-    print!("normal.inverse_cdf(p)=");
+    print!("normal.inverse_cdf(p) = ");
     out(x1, true);
+    let s = normal.sf(x0);
+    print!("normal.sf(x0) = ");
+    out(s, true);
+    let np = normal.cdf(-x0);
+    print!("normal.cdf(-x0) = ");
+    out(np, true);
+
 }
