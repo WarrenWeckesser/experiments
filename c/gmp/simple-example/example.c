21281e3202adfe8e660d0977d7a4e57c172653f9
Warren Weckesser <warren.weckesser@gmail.com>
2024-06-10 11:55:35 -0400

    c/gmp: Add a simple example.

---
+++ c/gmp/simple-example/example.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <gmp.h>
+
+int main()
+{
+    mpf_t x;
+    mpf_t y;
+    mpf_t z;
+
+    mpf_set_default_prec(256);
+
+    mpf_inits(x, y, z, NULL);
+
+    mpf_set_d(x, 1.0);
+    mpf_set_d(y, 3.0);
+    mpf_add(z, x, y);
+    printf("z = ");
+    mpf_out_str(stdout, 10, 0, z);
+    printf("\n");
+
+    mpf_clears(x, y, z, NULL);
+
+    return 0;
+}
