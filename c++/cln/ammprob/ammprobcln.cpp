6da1ef7a2cfcccb41d8f1631c44aecb52922789d
Warren Weckesser <warren.weckesser@gmail.com>
2024-06-11 18:18:49 -0400

    c++/cln: Tweak ammprobcln.cpp.

--- c++/cln/ammprob/ammprobcln.cpp
+++ c++/cln/ammprob/ammprobcln.cpp
@@ -1,17 +1,16 @@
-
 //
 //  ammprobcln.cpp
 //
 //  Calculation for a problem from the American Mathematical Monthly.
-//  (C++ version that uses the CLN library)
+//  Search for solutions to the equation
 //
-//  Warren Weckesser, Dept. of Mathematics, Colgate University
+//      n^3 + n^2 + n + 1 = x^2
+//
+//  This is a C++ version that uses the CLN library.
 //
 //
 //  Compile with:
 //     g++ ammprobcln.cpp -o ammprobcln -lcln
-//  or, if the library search options don't work,
-//     g++ ammprobcln.cpp -o ammprobcln -I$CLNDIR/include $CLNDIR/lib/libcln.a
 //
 
 #include <iostream>
@@ -21,38 +20,25 @@ using namespace std;
 using namespace cln;
 
 int main(int argc, char **argv)
-    {
-    //
-    // Declare the variables that we will use.  "cl_I" is the name of
-    // the integer class created by the CLN library.
-    //
+{
     cl_I n, nlast, m;
     cl_I root;
 
-    //
-    // Check that two values were given in the command line.
-    //
-    if (argc != 3)
-        {
+    if (argc != 3) {
         cout << "use: " << argv[0] << " first_integer last_integer\n";
         return(-1);
-        }
-    //
-    // Assign the numbers given on the command line to n and nlast.
-    //
-    n     = argv[1];    // First command-line argument
-    nlast = argv[2];    // Second command-line argument
-    //
-    // Check the integers from n to nlast.
-    //
-    while ( n <= nlast )
-        {
-        m = (n*n+1)*(n+1);
-        if (sqrtp(m, &root))
-            {
-            cout << n << " " << m << " " << root << endl;
-            }
-        n = n + 1;
-        }
-    return(0);
     }
+
+    n     = argv[1];
+    nlast = argv[2];
+
+    while (n <= nlast) {
+        m = (n*n + 1)*(n + 1);
+        if (sqrtp(m, &root)) {
+            cout << n << " " << m << " " << root << endl;
+        }
+        n = n + 1;
+    }
+
+    return(0);
+}
