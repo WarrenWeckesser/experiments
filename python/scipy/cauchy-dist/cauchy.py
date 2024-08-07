4bc0bea022f7ea9fe582332f3ab7eb7fdac76e78
Warren Weckesser <warren.weckesser@gmail.com>
2024-08-07 09:02:15 -0400

    python/scipy: tweak cauchy.py

--- python/scipy/cauchy-dist/cauchy.py
+++ python/scipy/cauchy-dist/cauchy.py
@@ -12,9 +12,11 @@ def cauchy_sf(x):
     return -np.arctan2(-1, x)/np.pi
 
 
-def cauchy_invcdf(p, loc=0, scale=1):
+# This works great for p < 0.5.  It loses precision as p -> 1.
+def cauchy_invcdf(p):
     return -1/np.tan(np.pi*p)
 
 
-def cauchy_invsf(p, loc=0, scale=1):
+# This works great for p > 0.5.  It loses precision as p -> 0.
+def cauchy_invsf(p):
     return 1/np.tan(np.pi*p)
