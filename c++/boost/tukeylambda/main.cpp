bc40aeef03de85c1569fbdefe39f77669baeff67
Warren Weckesser <warren.weckesser@gmail.com>
2024-08-12 23:26:58 -0400

    c++/boost: Clean up Tukey lambda code.

--- c++/boost/tukeylambda/main.cpp
+++ c++/boost/tukeylambda/main.cpp
@@ -5,12 +5,14 @@
 
 int main()
 {
-    double x = 2500;
-    double lam = -3;
+    double x = 9;
+    double lam = 0.05;
+
     double p = tukey_lambda_sf(x, lam);
     printf("p  = %25.16e\n", p);
-    double x1 = tukey_lambda_invsf(p, lam);
 
+    double x1 = tukey_lambda_invsf(p, lam);
     printf("x1 = %25.16e\n", x1);
+
     return std::isnan(p);
 }
--- c++/boost/tukeylambda/tukeylambda.h
+++ c++/boost/tukeylambda/tukeylambda.h
@@ -1,73 +1,56 @@
 
 #include <cmath>
-#include <cstdio>
 #include <limits>
 
 #include <boost/math/tools/roots.hpp>
 
 
 //
-// In toms748_solver, don't promote double to long double,
+// In toms748_solve, don't promote double to long double,
 // so we can have consistent behavior that does not depend
-// on the platform-dependent implementation of long double.
+// on platform-dependent implementations of long double.
 //
 typedef boost::math::policies::policy<
-    //boost::math::policies::promote_float<false>,
     boost::math::policies::promote_double<false>
 > Toms748Policy;
 
 
 //
-//  This function assumes:
-//  * p and lam are finite
-//  * 0 <= p <= 1
-//  * lam != 0
+// Inverse of the CDF of the Tukey lambda distribution.
 //
 double tukey_lambda_invcdf(double p, double lam)
 {
     double x;
-    // The values +/-1.7e308 are replacements for infinity.
-    // These are a hack to make toms748_solver work; it did
-    // not like getting infinity.
+
+    if (std::isnan(p) || !std::isfinite(lam) || p < 0 || p > 1) {
+        return std::numeric_limits<double>::quiet_NaN();
+    }
+
     if (p == 0) {
-        x = lam < 0 ? - 1.7e308: -1/lam;
+        x = lam <= 0 ? -INFINITY : -1/lam;
     }
     else if (p == 1) {
-        x = lam < 0 ? 1.7e308 : 1/lam;
+        x = lam <= 0 ? INFINITY : 1/lam;
     }
     else {
-        // This expression might need to be reformulated.
-        x = (std::pow(p, lam) - std::pow(1 - p, lam))/lam;
+        if (lam == 0) {
+            x = std::log(p) - std::log1p(-p);
+        }
+        else {
+            x = (std::pow(p, lam) - std::pow(1 - p, lam))/lam;
+        }
     }
-    //printf("  x = %25.15e\n", x);
     return x;
 }
 
 //
-//  This function assumes:
-//  * p and lam are finite
-//  * 0 <= p <= 1
-//  * lam != 0
+// Inverse of the survival function of the Tukey lambda distribution.
 //
 double tukey_lambda_invsf(double p, double lam)
 {
-    double x;
-    // The values +/-1.7e308 are replacements for infinity.
-    // These are a hack to make toms748_solver work; it did
-    // not like getting infinity.
-    if (p == 0) {
-        x = lam < 0 ? 1.7e308: 1/lam;
-    }
-    else if (p == 1) {
-        x = lam < 0 ? -1.7e308 : -1/lam;
-    }
-    else {
-        // This expression might need to be reformulated.
-        x = (std::pow(1 - p, lam) - std::pow(p, lam))/lam;
-    }
-    //printf("  x = %25.15e\n", x);
-    return x;
+    return -tukey_lambda_invcdf(p, lam);
 }
+
 
 std::pair<double, double>
 get_cdf_solver_bracket(double x, double lam)
@@ -112,7 +95,9 @@ get_cdf_solver_bracket(double x, double lam)
     return std::pair(pmin, pmax);
 }
 
-
+//
+// Cumulative distribution function of the Tukey lambda distribution.
+//
 double tukey_lambda_cdf(double x, double lam)
 {
     if (std::isnan(x) || !std::isfinite(lam)) {
@@ -144,8 +129,6 @@ double tukey_lambda_cdf(double x, double lam)
     }
 
     std::pair<double, double> initial_bracket = get_cdf_solver_bracket(x, lam);
-    printf("initial_bracket = (%26.16e, %26.16e)\n",
-           initial_bracket.first, initial_bracket.second);
     if (initial_bracket.first == 1.0) {
         return 1.0;
     }
@@ -156,14 +139,24 @@ double tukey_lambda_cdf(double x, double lam)
     boost::math::tools::eps_tolerance<double> tol;
     std::pair<double, double> bracket;
     std::uintmax_t max_iter = 100;
-    bracket = toms748_solve([x, lam](double p) {return tukey_lambda_invcdf(p, lam) - x;},
+    bracket = toms748_solve([x, lam](double p) {
+                                double x1 = tukey_lambda_invcdf(p, lam) - x;
+                                // Apparently toms748_solve does not handle inf, so
+                                // translate infs to very big numbers.
+                                if (std::isinf(x1)) {
+                                    x1 = std::copysign(std::numeric_limits<double>::max(), x1);
+                                }
+                                return x1;
+                            },
                             initial_bracket.first, initial_bracket.second,
                             tol, max_iter, Toms748Policy());
-    printf("bracket = (%26.16e, %26.16e)\n", bracket.first, bracket.second);
     if (bracket.first == bracket.second) {
         return bracket.first;
     }
     else {
+        // In some experiments, the result was better at an endpoint of the
+        // interval than in the middle, so here the endpoint with the better
+        // result is chosen to be the return value.
         double x1 = tukey_lambda_invcdf(bracket.first, lam);
         double x2 = tukey_lambda_invcdf(bracket.second, lam);
         if (std::abs(x - x1) < std::abs(x - x2)) {
@@ -175,64 +168,11 @@ double tukey_lambda_cdf(double x, double lam)
     }
 }
 
+
+//
+// Survival function of the Tukey lambda distribution.
+//
 double tukey_lambda_sf(double x, double lam)
 {
-    if (std::isnan(x) || !std::isfinite(lam)) {
-        return std::numeric_limits<double>::quiet_NaN();
-    }
-    if (std::isinf(x)) {
-        if (x > 0) {
-            return 0.0;
-        }
-        else {
-            return 1.0;
-        }
-    }
-    if (lam > 0) {
-        // When lam > 0, support is [-1/lam, 1/lam].
-        if (x >= 1/lam) {
-            return 0.0;
-        }
-        if (x <= -1/lam) {
-            return 1.0;
-        }
-    }
-    if (lam == 0) {
-        if (x >= 0) {
-            return std::exp(-x)/(1.0 + std::exp(-x));
-        } else {
-            return 1.0/(1.0 + std::exp(x));
-        }
-    }
-
-    std::pair<double, double> initial_bracket = get_cdf_solver_bracket(-x, lam);
-    printf("initial_bracket = (%26.16e, %26.16e)\n",
-           initial_bracket.first, initial_bracket.second);
-    if (initial_bracket.first == 1.0) {
-        return 1.0;
-    }
-    if (initial_bracket.second == 0.0) {
-        return 0.0;
-    }
-
-    boost::math::tools::eps_tolerance<double> tol;
-    std::pair<double, double> bracket;
-    std::uintmax_t max_iter = 80;
-    bracket = toms748_solve([x, lam](double p) {return tukey_lambda_invsf(p, lam) - x;},
-                            initial_bracket.first, initial_bracket.second,
-                            tol, max_iter, Toms748Policy());
-    printf("bracket = (%26.16e, %26.16e)\n", bracket.first, bracket.second);
-    if (bracket.first == bracket.second) {
-        return bracket.first;
-    }
-    else {
-        double x1 = tukey_lambda_invsf(bracket.first, lam);
-        double x2 = tukey_lambda_invsf(bracket.second, lam);
-        if (std::abs(x - x1) < std::abs(x - x2)) {
-            return bracket.first;
-        }
-        else {
-            return bracket.second;
-        }
-    }
+    return tukey_lambda_cdf(-x, lam);
 }
