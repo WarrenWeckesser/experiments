708574fd68869c00fe2e561ecc4a73ad08517182
Warren Weckesser <warren.weckesser@gmail.com>
2024-09-23 10:11:18 -0400

    c++/ginac: Tweak the example that checks for symbols in an expression.

--- c++/ginac/check-for-math-func/main.cpp
+++ c++/ginac/check-for-math-func/main.cpp
@@ -11,13 +11,16 @@ int main()
 
     for (int i=0; i<3; ++i)
         poly += factorial(i+16)*pow(x,i)*pow(y,2-i);
+    poly *= Pi;
 
     cout << poly << endl;
 
     bool uses_pow = poly.has(pow(wild(1), wild(2)));
     bool uses_atan2 = poly.has(atan2(wild(1), wild(2)));
+    bool uses_pi = poly.has(Pi);
     cout << "poly uses pow:   " << uses_pow << endl;
     cout << "poly uses atan2: " << uses_atan2 << endl;
+    cout << "poly uses Pi:    " << uses_pi << endl;
 
     return 0;
 }
