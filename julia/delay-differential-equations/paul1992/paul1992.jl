3f6bb22c843f5877d84dce90b4c4c828bb594cef
Warren Weckesser <warren.weckesser@gmail.com>
2024-09-28 21:52:29 -0400

    julia: update plot of the Paul (1992) state-dependent delay example

--- julia/delay-differential-equations/paul1992/paul1992.jl
+++ julia/delay-differential-equations/paul1992/paul1992.jl
@@ -7,6 +7,19 @@ using Plots
 # source of the equation as "Developing a Delay Differential Equation Solver" by
 # C. A. H. Paul (J. Appl. Num. Math., Vol. 9, pp. 403-414, 1992). 
 
+function exact_solution(t)
+    # This function assumes 0 <= t <= 10
+    e = Base.MathConstants.e
+    if t <= e - 1
+        t + 1
+    elseif t <= e*e -1
+        exp((t + 1)/e)
+    else
+        (e/(3 - log(t + 1)))^e
+    end
+end
+
+
 function paul1992!(du, u, h, p, t)
     y = u[1]
     delayedy = h(p, log(y) - 1)[1]
@@ -24,5 +37,12 @@ prob = DDEProblem(paul1992!, u0, history, tspan; dependent_lags = [lag])
 alg = MethodOfSteps(Tsit5())
 sol = solve(prob, alg, abstol = 1e-11, reltol = 1e-8)
 
-plot(sol, title = "State-dependent delay example", xaxis = "t", yaxis = "x", legend = false)
-savefig("state_dependent_delay_solution.svg")
+plot(sol, title = "State-dependent delay example", xaxis = "t", yaxis = "y",
+     color = "black", label = "Computed solution")
+
+t = range(0.0, 10.0, 501)
+y = [exact_solution(t1) for t1 = t]
+
+plot!(t, y, label="Exact solution", linewidth = 5, alpha = 0.4)
+
+savefig("paul1992.svg")
