48b45f28a2ff36bcc76a74dfae9b2cdb56185c4a
Warren Weckesser <warren.weckesser@gmail.com>
2024-10-13 15:44:33 -0400

    c++/boost/tukeylambda: Use a common function to handle edge cases in the various invcdf functions.

--- c++/boost/tukeylambda/tukeylambda.h
+++ c++/boost/tukeylambda/tukeylambda.h
@@ -7,9 +7,8 @@
 #include <boost/math/special_functions/logaddexp.hpp>
 
 //
-// In toms748_solve, don't promote double to long double,
-// so we can have consistent behavior that does not depend
-// on platform-dependent implementations of long double.
+// In toms748_solve, don't promote double to long double, so we
+// can have consistent behavior across different platforms.
 //
 typedef boost::math::policies::policy<
     boost::math::policies::promote_double<false>
@@ -41,32 +40,62 @@ double logistic_invcdf(double p)
     return std::log1p(s) - std::log1p(-s);
 }
 
+
+//
+// Handle the edges cases of the Tukey lambda distribution inverse cdf.
+// If the calculation is handled by this function, *result is assigned
+// and 1 is returned.  Otherwise 0 is returned.
+//
+int tukey_lambda_invcdf_edge_case(double p, double lam, double *result)
+{
+    if (std::isnan(p) || !std::isfinite(lam) || p < 0 || p > 1) {
+        *result = std::numeric_limits<double>::quiet_NaN();
+        return 1;
+    }
+    if (p == 0) {
+        *result = lam <= 0 ? -std::numeric_limits<double>::infinity() : -1/lam;
+        return 1;
+    }
+    if (p == 1) {
+        *result = lam <= 0 ? std::numeric_limits<double>::infinity() : 1/lam;
+        return 1;
+    }
+    if (p == 0.5) {
+        return 0.0;
+    }
+    if (lam == 0) {
+        *result = logistic_invcdf(p);
+        return 1;
+    }
+    const double log_max = std::log(std::numeric_limits<double>::max());
+    if (lam < 0) {
+        if (p > 0.5 && lam*std::log1p(-p) > log_max) {
+            // Overflow of (1 - p)**lam.
+            *result = std::numeric_limits<double>::infinity();
+            return 1;
+        }
+        if (p < 0.5 && lam*std::log(p) > log_max) {
+            // Overflow of p**lam.
+            *result = -std::numeric_limits<double>::infinity();
+            return 1;
+        }
+    }
+    return 0;
+}
+
 //
 // Inverse of the CDF of the Tukey lambda distribution.
+// This is the direct implementation of the formula, i.e. the "naive"
+// implementation. ("Naive" doesn't mean "bad"; it works fine for a wide
+// range of parameter values.)
 //
 double tukey_lambda_invcdf(double p, double lam)
 {
     double x;
-
-    if (std::isnan(p) || !std::isfinite(lam) || p < 0 || p > 1) {
-        return std::numeric_limits<double>::quiet_NaN();
+    if (tukey_lambda_invcdf_edge_case(p, lam, &x)) {
+        return x;
     }
-
-    if (p == 0) {
-        x = lam <= 0 ? -INFINITY : -1/lam;
-    }
-    else if (p == 1) {
-        x = lam <= 0 ? INFINITY : 1/lam;
-    }
-    else {
-        if (lam == 0) {
-            x = logistic_invcdf(p);
-        }
-        else {
-            x = (std::pow(p, lam) - std::pow(1 - p, lam))/lam;
-        }
-    }
-    return x;
+    return (std::pow(p, lam) - std::pow(1 - p, lam))/lam;
 }
 
 static inline double
@@ -78,29 +107,15 @@ pow1p(double x, double r)
 double tukey_lambda_invcdf2(double p, double lam)
 {
     double x;
-
-    if (std::isnan(p) || !std::isfinite(lam) || p < 0 || p > 1) {
-        return std::numeric_limits<double>::quiet_NaN();
+    if (tukey_lambda_invcdf_edge_case(p, lam, &x)) {
+        return x;
     }
-
-    if (p == 0) {
-        x = lam <= 0 ? -INFINITY : -1/lam;
-    }
-    else if (p == 1) {
-        x = lam <= 0 ? INFINITY : 1/lam;
+    if (p > 0.3 && p < 0.7) {
+        double t = 2*p - 1.0;
+        x = std::pow(2.0, -lam)*(pow1p(t, lam) - pow1p(-t, lam))/lam;
     }
     else {
-        if (lam == 0) {
-            x = logistic_invcdf(p);
-        }
-        else if (p > 0.3 && p < 0.7) {
-            double t = 2*p - 1.0;
-            x = std::pow(2.0, -lam)*(pow1p(t, lam) - pow1p(-t, lam))/lam;
-
-        }
-        else {
-            x = (std::pow(p, lam) - std::pow(1 - p, lam))/lam;
-        }
+        x = (std::pow(p, lam) - std::pow(1 - p, lam))/lam;
     }
     return x;
 }
@@ -108,54 +123,22 @@ double tukey_lambda_invcdf2(double p, double lam)
 double tukey_lambda_invcdf3a(double p, double lam)
 {
     double x;
-
-    if (std::isnan(p) || !std::isfinite(lam) || p < 0 || p > 1) {
-        return std::numeric_limits<double>::quiet_NaN();
+    if (tukey_lambda_invcdf_edge_case(p, lam, &x)) {
+        return x;
     }
-
-    if (p == 0) {
-        x = lam <= 0 ? -INFINITY : -1/lam;
-    }
-    else if (p == 1) {
-        x = lam <= 0 ? INFINITY : 1/lam;
-    }
-    else {
-        if (lam == 0) {
-            x = logistic_invcdf(p);
-        }
-        else {
-            double t = lam*logistic_invcdf(p);
-            x = -std::pow(p, lam)*std::expm1(-t)/lam;
-        }
-    }
-    return x;
+    double t = lam*logistic_invcdf(p);
+    return -std::pow(p, lam) * std::expm1(-t)/lam;
 }
 
 
 double tukey_lambda_invcdf3b(double p, double lam)
 {
     double x;
-
-    if (std::isnan(p) || !std::isfinite(lam) || p < 0 || p > 1) {
-        return std::numeric_limits<double>::quiet_NaN();
+    if (tukey_lambda_invcdf_edge_case(p, lam, &x)) {
+        return x;
     }
-
-    if (p == 0) {
-        x = lam <= 0 ? -INFINITY : -1/lam;
-    }
-    else if (p == 1) {
-        x = lam <= 0 ? INFINITY : 1/lam;
-    }
-    else {
-        if (lam == 0) {
-            x = logistic_invcdf(p);
-        }
-        else {
-            double t = lam*logistic_invcdf(p);
-             x = pow1p(-p, lam)*std::expm1(t)/lam;
-        }
-    }
-    return x;
+    double t = lam*logistic_invcdf(p);
+    return pow1p(-p, lam)*std::expm1(t)/lam;
 }
 
 #define TUKEY_LAMBDA_INVCDF_TAYLOR_MAX_N 31
@@ -163,50 +146,41 @@ double tukey_lambda_invcdf3b(double p, double lam)
 double tukey_lambda_invcdf_taylor(double p, double lam, int n)
 {
     double x;
+    if (tukey_lambda_invcdf_edge_case(p, lam, &x)) {
+        return x;
+    }
 
     if (n > TUKEY_LAMBDA_INVCDF_TAYLOR_MAX_N) {
         return std::numeric_limits<double>::quiet_NaN();
     }
-    if (std::isnan(p) || !std::isfinite(lam) || p < 0 || p > 1) {
-        return std::numeric_limits<double>::quiet_NaN();
+    x = logistic_invcdf(p);
+    // lam == 0 is handled above by tukey_lambda_edge_case(), so
+    // so we know lam != 0 here.
+    std::array<double, TUKEY_LAMBDA_INVCDF_TAYLOR_MAX_N+1> logp_powers, logq_powers;
+    double logp = std::log(p);
+    double logq = std::log1p(-p);
+    double logp_power = 1.0;
+    double logq_power = 1.0;
+    for (int k = 0; k <= n; ++k) {
+        logp_powers[k] = logp_power;
+        logq_powers[k] = logq_power;
+        logp_power *= logp;
+        logq_power *= logq;
     }
-
-    if (p == 0) {
-        x = lam <= 0 ? -INFINITY : -1/lam;
-    }
-    else if (p == 1) {
-        x = lam <= 0 ? INFINITY : 1/lam;
-    }
-    else {
-        x = logistic_invcdf(p);
-        if (lam != 0) {
-            std::array<double, TUKEY_LAMBDA_INVCDF_TAYLOR_MAX_N+1> logp_powers, logq_powers;
-            double logp = std::log(p);
-            double logq = std::log1p(-p);
-            double logp_power = 1.0;
-            double logq_power = 1.0;
-            for (int k = 0; k <= n; ++k) {
-                logp_powers[k] = logp_power;
-                logq_powers[k] = logq_power;
-                logp_power *= logp;
-                logq_power *= logq;
-            }
-            double sum = 0.0;
-            double fac = 1.0;
-            double lamp = 1.0;
-            for (int k = 0; k <= n; ++k) {
-                double c = lamp/fac;
-                double inner_sum = 0.0;
-                for (int j = 0; j <= k; ++j) {
-                    inner_sum += logp_powers[k - j]*logq_powers[j];
-                }
-                sum += c*inner_sum;
-                lamp *= lam;
-                fac *= k + 2.0;
-            }
-            x *= sum;
+    double sum = 0.0;
+    double fac = 1.0;
+    double lamp = 1.0;
+    for (int k = 0; k <= n; ++k) {
+        double c = lamp/fac;
+        double inner_sum = 0.0;
+        for (int j = 0; j <= k; ++j) {
+            inner_sum += logp_powers[k - j]*logq_powers[j];
         }
+        sum += c*inner_sum;
+        lamp *= lam;
+        fac *= k + 2.0;
     }
+    x *= sum;
     return x;
 }
 
@@ -217,24 +191,12 @@ double tukey_lambda_invcdf_taylor(double p, double lam, int n)
 double tukey_lambda_invcdf_experimental(double p, double lam)
 {
     double x;
-    double q = 1 - p;
-
-    if (std::isnan(p) || !std::isfinite(lam) || p < 0 || p > 1) {
-        return std::numeric_limits<double>::quiet_NaN();
-    }
-    if (p == 0) {
-        return lam <= 0 ? -INFINITY : -1/lam;
-    }
-    if (p == 1) {
-        return lam <= 0 ? INFINITY : 1/lam;
-    }
-    if (p == 0.5) {
-        return 0.0;
-    }
-    x = logistic_invcdf(p);
-    if (lam == 0.0) {
+    if (tukey_lambda_invcdf_edge_case(p, lam, &x)) {
         return x;
     }
+
+    x = logistic_invcdf(p);
+    double q = 1 - p;
     int k = 1;
     while (k < 100) {
         lam = lam/2.0;
@@ -445,7 +407,7 @@ double tukey_lambda_logpdf(double x, double lam)
     if (lam > 0) {
         double recip_lam = 1.0/lam;
         if (x < -recip_lam || x > recip_lam) {
-            return -INFINITY;
+            return -std::numeric_limits<double>::infinity();
         }
     }
     double p_smaller;
