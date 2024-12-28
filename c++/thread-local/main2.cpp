54df62f96eae361cae1e95969381a4133e51eba5
Warren Weckesser <warren.weckesser@gmail.com>
2024-12-27 22:33:59 -0500

    c++/thread-local: Remove redundant 'static' specifier.

--- c++/thread-local/main2.cpp
+++ c++/thread-local/main2.cpp
@@ -22,7 +22,12 @@ double calc(double c[]) {
 
 void thread_func(int id)
 {
-    thread_local static double c[num_coefficients];
+    // `thread_local` here implies `static`; according to the C++ standard
+    // (e.g. https://port70.net/%7Ensz/c/c%2B%2B/c%2B%2B11_n3337.txt),
+    // "When thread_local is applied to a variable of block scope the
+    // storage-class-specifier static is implied if it does not appear
+    // explicitly."
+    thread_local double c[num_coefficients];
     initialize_c(c, id);
     double result = calc(c);
     printf("id %3d: result = %12.0f  (%12.0f)\n", id, result, result - id*num_coefficients);
