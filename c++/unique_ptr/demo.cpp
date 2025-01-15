fe4b8628837de326b2e36fed50d497457186dcde
Warren Weckesser <warren.weckesser@gmail.com>
2025-01-14 19:13:22 -0500

    c++/unique_ptr: add a variation with array initialization in demo.cpp

--- c++/unique_ptr/demo.cpp
+++ c++/unique_ptr/demo.cpp
@@ -16,6 +16,7 @@ double sum(size_t n, double *x)
 
 double do_the_thing(size_t n)
 {
+    // Allocate an array and wrap it in a unique_ptr.
     auto values = std::unique_ptr<double[]>{new (std::nothrow) double[n]};
     if (values.get() == nullptr) {
         fprintf(stderr, "memory allocation failed\n");
@@ -28,9 +29,25 @@ double do_the_thing(size_t n)
     return total;
 }
 
+double do_the_thing2(size_t n)
+{
+    // Allocate an array initialized to 0 and wrap it in a unique_ptr.
+    auto values = std::unique_ptr<double[]>{new (std::nothrow) double[n]()};
+    if (values.get() == nullptr) {
+        fprintf(stderr, "memory allocation failed\n");
+        return std::numeric_limits<double>::quiet_NaN();
+    }
+    for (size_t i = 1; i < n; ++i) {
+        values[i] = i;
+    }
+    double total = sum(n, values.get());
+    return total;
+}
 
 int main(int argc, char *argv[])
 {
-    double foo = do_the_thing(1000);
-    printf("foo = %12.0f\n", foo);
+    double foo = do_the_thing(10000);
+    printf("foo  = %12.0f\n", foo);
+    double foo2 = do_the_thing2(10000);
+    printf("foo2 = %12.0f\n", foo2);
 }
