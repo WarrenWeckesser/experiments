a96d2b5c685ec9f1150a4f9fda3cb9108615038f
Warren Weckesser <warren.weckesser@gmail.com>
2025-10-26 22:06:09 -0400

    Work on notes for the Zipfian rejection method.

--- python/numpy/random-cython/docs/zipfian_plots.py
+++ python/numpy/random-cython/docs/zipfian_plots.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import zipfian
+from scipy.special import boxcox
 
 def zipfian_pdf(x, a, n):
     # Treat the discrete distribution as a piecewise constant
@@ -18,6 +19,27 @@ def h(x, a, n):
     mask = (x > 0.5) & (x < n + 0.5)
     out = np.zeros_like(x)
     out[mask] = np.round(x[mask])**-a
+    return out
+
+def g(x, a, n):
+    # g is the "hat" function
+    out = np.zeros(len(x))
+    out[(0.5 <= x) & (x <= 1.5)] = 1.0
+    mask = (1.5 < x) & (x < n + 0.5)
+    out[mask] = (x[mask] - 0.5)**-a
+    return out
+
+
+def G(x, a, n):
+    # Currently, the only call of this function used in the
+    # rejection method is G(n + 0.5, a, n).
+    x = np.atleast_1d(x)
+    out = np.zeros(len(x))
+    out[x >= n + 0.5] = boxcox(n, 1 - a) + 1
+    mask1 = (0.5 <= x) & (x <= 1.5)
+    out[mask1] = x[mask1] - 0.5
+    mask = (1.5 < x) & (x < n + 0.5)
+    out[mask] = boxcox(x[mask] - 0.5, 1 - a) + 1
     return out
 
 
@@ -46,8 +68,7 @@ pdf = zipfian_pdf(xx, a, n)
 
 plt.figure(figsize=figsize)
 
-plt.plot(xx, pdf,
-         label='Zipfian "PDF"')
+plt.plot(xx, pdf)
 
 plt.grid(visible=True)
 plt.xlabel('x')
@@ -56,15 +77,49 @@ plt.savefig('zipfian_pdf.png')
 
 # Figure 3. Nonnormalized PDF
 
-
 nnpdf = h(xx, a, n)
 
 plt.figure(figsize=figsize)
 
-plt.plot(xx, nnpdf,
-         label='Zipfian Nonnormalized PDF')
+plt.plot(xx, nnpdf)
 
 plt.grid(visible=True)
 plt.xlabel('x')
-plt.title(f'Zipfian PDF (a={a}, n={n})')
+plt.title(f'Zipfian Nonnormalized PDF (a={a}, n={n})')
 plt.savefig('zipfian_nnpdf.png')
+
+# Figure 4. Nonnormalized PDF and the dominating function.
+
+dom = g(xx, a, n)
+
+plt.figure(figsize=figsize)
+
+plt.plot(xx, nnpdf, label='Nonnormalized PDF')
+plt.plot(xx, dom, '--', label='Dominating function')
+
+plt.legend(shadow=True, framealpha=1)
+plt.grid(visible=True)
+plt.xlabel('x')
+plt.title(f'Zipfian Nonnormalized PDF and Dominating Function (a={a}, n={n})')
+plt.savefig('zipfian_nnpdf_and_dom.png')
+
+# Figure 5. G(x, a, n)
+
+plt.figure(figsize=figsize)
+
+plt.plot(xx, G(xx, a, n), 'k',
+         label='G(x, a, n)\nintegral of the hat function g(x, a, n)')
+plt.plot(0.5, 0, 'k.')
+plt.plot(1.5, G(1.5, a, n), 'k.')
+maxG = G(n + 0.5, a, n)
+plt.plot(n + 0.5, maxG, 'k.')
+plt.axhline(maxG, linestyle=':', alpha=0.5,
+            label='max G(x, a, n) = G(n + 0.5, a, n)')
+
+plt.legend(shadow=True, framealpha=1)
+plt.grid(visible=True)
+plt.xlabel('x')
+plt.title(f'Zipfian Rejection Method  (a={a}, n={n})\n'
+          'G(x, a, n), the nonnormalized CDF of the dominating distribution')
+
+plt.savefig('zipfian_dom_nncdf.png')
--- python/numpy/random-cython/docs/zipfian_rejection_method.md
+++ python/numpy/random-cython/docs/zipfian_rejection_method.md
@@ -16,6 +16,35 @@ Extend this distribution to a continuous distribution with a piecewise constant 
 ![](https://github.com/WarrenWeckesser/experiments/blob/main/python/numpy/random-cython/docs/zipfian_pdf.png)
 
 Scale up by removing the normalization constant, so the value at $x = k$ is just $k^{-a}$.
-This is what I'll call the "target" function.
+This is what I'll call the "target" function $h(x, a, n)$.
 
 ![](https://github.com/WarrenWeckesser/experiments/blob/main/python/numpy/random-cython/docs/zipfian_nnpdf.png)
+
+The *dominating distribution* is distribution with a nonnormalized PDF $g(x, a, n)$ that
+satisfies $g(x, a, n) \ge h(x, a, n)$ on the support.  This is the distribution that we'll
+use to generate candidate random variates.  For the Zipfian distribution, we can use
+
+$$
+    g(x, a, n) = x^{-a}
+     = \begin{cases}
+         1      & \frac{1}{2} \le x < \frac{3}{2} & \\
+         x^{-a} & \frac{3}{2} \le x < n + \frac{1}{2} \\
+         0      & \textrm{otherwise}
+       \end{cases}
+$$
+
+This plot shows the target nonnormalized PDF and the dominating PDF.
+
+![](https://github.com/WarrenWeckesser/experiments/blob/main/python/numpy/random-cython/docs/zipfian_nnpdf_and_dom.png)
+
+To generate variates from the dominating distribution, we'll use the inversion method: generate uniform
+variates and pass them through the inverse of the CDF.  To avoid having to deal with a normalization
+constant, we can get the nonnormalized CDF $G(x, a, n)$ by integrating the nonnormalized PDF.
+
+![](https://github.com/WarrenWeckesser/experiments/blob/main/python/numpy/random-cython/docs/zipfian_dom_nncdf.png)
+
+
+To generate random variates with the inversion method from $G(x, a, n)$, we don't have to normalized it
+and get a true CDF. Instead, we generate uniform variates $U$ from the interval $[0, G(n+1/2, a, n]$ (since
+$G(n+1/2, a, n)$ is the value of $G(x, a, n)$ at the right end of the support).  Then a random variate
+from the dominating distribution is $G^{-1}(U, a, n)$.
