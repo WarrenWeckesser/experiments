048b9720ec3b96d789a43f1dca98c008efdd09d0
Warren Weckesser <warren.weckesser@gmail.com>
2025-10-29 16:11:30 -0400

    c++/xsf: Add a program to run cyl_bessel_k().

---
+++ c++/xsf/kv/cyl_bessel_k_demo.cpp
@@ -0,0 +1,32 @@
+// Requires at least C++17.
+
+#include <complex>
+#include <iomanip>
+#include <iostream>
+#include <xsf/bessel.h>
+
+int main(int argc, char *argv[])
+{
+    using namespace std;
+    using namespace std::complex_literals;
+
+    double nu = 0.0;
+    double re = 0.0;
+    double im = 0.0;
+
+    if (argc > 1) {
+        nu = std::stod(argv[1]);
+        if (argc > 2) {
+            re = std::stod(argv[2]);
+            if (argc > 3) {
+                im = std::stod(argv[3]);
+            }
+        }
+    }
+    complex<double> z(re, im);
+    complex<double> w = xsf::cyl_bessel_k(nu, z);
+
+    cout << setprecision(15);
+    cout << w.real() << " " << w.imag() << endl;
+    return 0;
+}
