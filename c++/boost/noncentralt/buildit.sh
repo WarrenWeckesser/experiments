af982eaa36d292e531d13dd298ba1eb69de1d91c
Warren Weckesser <warren.weckesser@gmail.com>
2025-11-15 14:10:56 -0500

    c++/boost/noncentralt: Tweak c++ arguments in buildit.sh

--- c++/boost/noncentralt/buildit.sh
+++ c++/boost/noncentralt/buildit.sh
@@ -1,2 +1,2 @@
-g++ -std=c++1z noncentralt_mean_var.cpp -o noncentralt_mean_var
-g++ -std=c++1z noncentralt_pdf.cpp -o noncentralt_pdf
+g++ -std=c++17 noncentralt_mean_var.cpp -o noncentralt_mean_var
+g++ -std=c++17 noncentralt_pdf.cpp -o noncentralt_pdf
