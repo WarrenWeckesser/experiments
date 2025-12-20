00eee18854231657236952776d9a596f157b60db
Warren Weckesser <warren.weckesser@gmail.com>
2025-12-19 21:43:51 -0500

    c++/xsf: add command-line demo for cyl_bessel_je

---
+++ c++/xsf/cyl_bessel_je/cyl_bessel_je_demo.cpp
@@ -0,0 +1,31 @@
+// Requires at least C++17.
+
+#include <complex>
+#include <iomanip>
+#include <iostream>
+#include <xsf/bessel.h>
+
+int main(int argc, char *argv[])
+{
+    using namespace std;
+
+    double nu = 0.0;
+    double re = 0.0;
+    double im = 0.0;
+
+    if (argc > 1) {
+        nu = stod(argv[1]);
+        if (argc > 2) {
+            re = stod(argv[2]);
+            if (argc > 3) {
+                im = stod(argv[3]);
+            }
+        }
+    }
+    complex<double> z(re, im);
+    complex<double> w = xsf::cyl_bessel_je(nu, z);
+
+    cout << setprecision(15);
+    cout << w.real() << " " << w.imag() << endl;
+    return 0;
+}
