5eb8ee7cd324600ecc6fd34798c612cb610d3203
Warren Weckesser <warren.weckesser@gmail.com>
2026-07-17 16:30:02 -0400

    python/mpsci: sympy work for mode of Benktander 2 distr.

---
+++ python/mpsci/benktander2-work/find_mode.py
@@ -0,0 +1,43 @@
+import sympy
+from sympy import init_printing
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+init_printing()
+
+x, a, b = sympy.symbols('x a b')
+# PDF:
+f = sympy.exp((a / b)*(1 - x**b)) * x**(b - 2) * (a * x**b - b + 1)
+df = sympy.diff(f, x)
+mode = sympy.solve(df, x)
+print(f"mode: {mode}")
+if len(mode) != 1:
+    print(f"({len(mode)} formulas)")
+for m in mode:
+    print()
+    sympy.pprint(m)
+
+# pdf = sympy.lambdify((x, a, b), f, "numpy")
+
+# aa = 12
+# bb = 30
+# xx = np.linspace(1, 1.5, 1000)
+# yy = pdf(xx, aa, bb)
+
+# mode0 = sympy.lambdify((a, b), mode[0], "numpy")
+# mode1 = sympy.lambdify((a, b), mode[1], "numpy")
+# mode2 = sympy.lambdify((a, b), mode[2], "numpy")
+# m0 = mode0(aa, bb)
+# m1 = mode1(aa, bb)
+# m2 = mode2(aa, bb)
+
+# print(f"{m0 = }")
+# print(f"{m1 = }")
+# print(f"{m2 = }")
+
+# m = max(1, m2)
+# plt.plot(xx, yy)
+# plt.plot(m, pdf(m, aa, bb), '.')
+# plt.grid(True)
+# plt.show()
