cb614e5096a0c1996e1cc6aee575ca548fb5adbe
Warren Weckesser <warren.weckesser@gmail.com>
2026-07-18 14:26:20 -0400

    c++/boost/noncentralt: Add a CDF demo.

--- c++/boost/noncentralt/buildit.sh
+++ c++/boost/noncentralt/buildit.sh
@@ -1,2 +1,3 @@
-g++ -std=c++17 noncentralt_mean_var.cpp -o noncentralt_mean_var
-g++ -std=c++17 noncentralt_pdf.cpp -o noncentralt_pdf
+g++ -std=c++17 -I$BOOSTDIR noncentralt_mean_var.cpp -o noncentralt_mean_var
+g++ -std=c++17 -I$BOOSTDIR noncentralt_pdf.cpp -o noncentralt_pdf
+g++ -std=c++17 -I$BOOSTDIR noncentralt_cdf.cpp -o noncentralt_cdf
---
+++ c++/boost/noncentralt/noncentralt_cdf.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <boost/math/distributions/non_central_t.hpp>
+
+using namespace std;
+using namespace boost::math::policies;
+using boost::math::non_central_t_distribution;
+
+typedef policy<promote_double<false>> no_double_promotion;
+typedef policy<promote_double<true>> double_promotion;
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4) {
+        cout << "use: " << argv[0] << " x df nc\n";
+        return -1;
+    }
+
+    double x = stod(argv[1]);
+    double df = stod(argv[2]);
+    double nc = stod(argv[3]);
+
+    cout << "x  = " << x << endl;
+    cout << "df = " << df << endl;
+    cout << "nc = " << nc << endl;
+    auto nct1 = non_central_t_distribution<double, no_double_promotion>(df, nc);
+    double p1 = cdf(nct1, x);
+    cout << "p  = " << scientific << setw(23) << setprecision(16) << p1
+         << " (with no_double_promotion policy)" << endl;
+
+    auto nct2 = non_central_t_distribution<double, double_promotion>(df, nc);
+    double p2 = cdf(nct2, x);
+    cout << "p  = " << scientific << setw(23) << setprecision(16) << p2
+         << " (with double_promotion policy)" << endl;
+
+    return 0;
+}
